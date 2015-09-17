3de2a8eb3e628e0b24f1a2a87d422dc5a4223e7a
Lee Byron <lee@leebyron.com>
2015-09-16 20:44:26 -0700

    use non minified for example

--- example/build.sh
+++ example/build.sh
@@ -2,6 +2,6 @@
 
 rm -rf dist/ && mkdir -p dist/ &&
 babel server.js --optional runtime -o dist/server.js &&
-cp node_modules/graphiql/graphiql.min.js dist/graphiql.min.js &&
+cp node_modules/graphiql/graphiql.js dist/graphiql.js &&
 cp node_modules/graphiql/graphiql.css dist/graphiql.css &&
 cat index.html > dist/index.html
