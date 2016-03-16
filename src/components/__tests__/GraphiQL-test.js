c7dd7dc96fbc27caf4cf6b100b649af2546c74d4
Lee Byron <lee@leebyron.com>
2016-03-16 12:36:22 -0700

    Clean up tests

--- src/components/__tests__/GraphiQL-test.js
+++ src/components/__tests__/GraphiQL-test.js
@@ -1,27 +1,39 @@
+/**
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the license found in the
+ *  LICENSE-examples file in the root directory of this source tree.
+ */
+
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
-
 import React from 'react';
-import TestUtils from 'react-addons-test-utils';
+import { renderIntoDocument } from 'react-addons-test-utils';
 
 import { GraphiQL } from '../GraphiQL';
 
-const render = props =>
-  () => TestUtils.renderIntoDocument(React.createElement(GraphiQL, props));
-
-const createFetcher = () => () => {};
 
 describe('GraphiQL', () => {
-  it('should throw error without fetcher', () => {
-    expect(render()).to.throw('GraphiQL requires a fetcher function');
-  });
+  const noOpFetcher = () => {};
 
-  it('should not throw error if schema missing and query provided', () => {
-    const renderFn = render({ fetcher: createFetcher(), query: '{}'});
-    expect(renderFn).to.not.throw(Error);
+  it('should throw error without fetcher', () => {
+    expect(() => renderIntoDocument(
+      <GraphiQL />
+    )).to.throw(
+      'GraphiQL requires a fetcher function'
+    );
   });
 
   it('should construct correctly with fetcher', () => {
-    expect(render({ fetcher: createFetcher() })).to.not.throw(Error);
+    expect(() => renderIntoDocument(
+      <GraphiQL fetcher={noOpFetcher} />
+    )).to.not.throw();
+  });
+
+  it('should not throw error if schema missing and query provided', () => {
+    expect(() => renderIntoDocument(
+      <GraphiQL fetcher={noOpFetcher} query="{}" />
+    )).to.not.throw();
   });
 });
