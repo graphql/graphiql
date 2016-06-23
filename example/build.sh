2365914020348a7ad15eb89e813a46da066fb9ba
Hyohyeon Jeong <asiandrummer@fb.com>
2016-06-23 12:12:08 -0700

    add fallback from cdn to local copies of dependencies in example

--- example/build.sh
+++ example/build.sh
@@ -4,4 +4,5 @@ rm -rf dist/ && mkdir -p dist/ &&
 babel server.js -o dist/server.js &&
 cp node_modules/graphiql/graphiql.js dist/graphiql.js &&
 cp node_modules/graphiql/graphiql.css dist/graphiql.css &&
+cp -r vendor/ dist/vendor/ &&
 cat index.html > dist/index.html
