3ff8b4c41e2946357952a96666c2c0c13eb2476d
Joel Griffith <jgriffith@appnexus.com>
2016-11-04 14:38:56 -0700

    Saving doc-exploreer status into storage, loading on mount

--- src/components/GraphiQL.js
+++ src/components/GraphiQL.js
@@ -169,7 +169,8 @@ export class GraphiQL extends React.Component {
       variableEditorOpen: Boolean(variables),
       variableEditorHeight:
         Number(this._storageGet('variableEditorHeight')) || 200,
-      docExplorerOpen: false,
+      docExplorerOpen:
+        (this._storageGet('docExplorerOpen') === 'true') || false,
       docExplorerWidth: Number(this._storageGet('docExplorerWidth')) || 350,
       isWaitingForResponse: false,
       subscription: null,
@@ -252,6 +253,7 @@ export class GraphiQL extends React.Component {
     this._storageSet('editorFlex', this.state.editorFlex);
     this._storageSet('variableEditorHeight', this.state.variableEditorHeight);
     this._storageSet('docExplorerWidth', this.state.docExplorerWidth);
+    this._storageSet('docExplorerOpen', this.state.docExplorerOpen);
   }
 
   render() {
