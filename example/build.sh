f9de00729998225a0e7659e453ec4e7c0bdddf71
Joel Griffith <mrskitch@gmail.com>
2016-11-07 11:20:34 -0800

    Adds in search functionality to result-viewer, bugfixes for example (#188)
    * Adds in search functionality to result-viewer, bugfixes for example
    * Fixing server now that client queries appropriate endpoint
    * Removing graphiql from deps as it's now available at build time from
    root

--- example/build.sh
+++ example/build.sh
@@ -2,7 +2,7 @@
 
 rm -rf dist/ && mkdir -p dist/ &&
 babel server.js -o dist/server.js &&
-cp node_modules/graphiql/graphiql.js dist/graphiql.js &&
-cp node_modules/graphiql/graphiql.css dist/graphiql.css &&
+cp ../graphiql.js dist/graphiql.js &&
+cp ../graphiql.css dist/graphiql.css &&
 cp -r vendor/ dist/vendor/ &&
 cat index.html > dist/index.html
