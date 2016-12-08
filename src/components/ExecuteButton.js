1eeb36cba853fe752f739a0d93f3bbf93e3f8598
Greg Hurrell <greg@hurrell.net>
2016-12-08 13:28:41 -0800

    Fix lint errror in ExecuteButton.js
    Travis is currently complaining about this ([example build](https://travis-ci.org/graphql/graphiql/builds/181706048)).

--- src/components/ExecuteButton.js
+++ src/components/ExecuteButton.js
@@ -71,7 +71,7 @@ export class ExecuteButton extends React.Component {
 
     const pathJSX = this.props.isRunning ?
       <path d="M 10 10 L 23 10 L 23 23 L 10 23 z" /> :
-        <path d="M 11 9 L 24 16 L 11 23 z" />;
+      <path d="M 11 9 L 24 16 L 11 23 z" />;
 
     return (
       <div className="execute-button-wrap">
