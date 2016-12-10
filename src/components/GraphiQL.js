462303084a9e388c7d9b17831bfa7dc940bfbd4f
Greg Hurrell <greg@hurrell.net>
2016-12-09 17:21:50 -0800

    Hide parent type name when doc explorer is narrow
    As suggested by @leebyron here:
    
      https://github.com/graphql/graphiql/pull/244#issuecomment-266163750

--- src/components/GraphiQL.js
+++ src/components/GraphiQL.js
@@ -278,6 +278,10 @@ export class GraphiQL extends React.Component {
       display: this.state.docExplorerOpen ? 'block' : 'none',
       width: this.state.docExplorerWidth,
     };
+    const docExplorerWrapClasses = [
+      'docExplorerWrap',
+      this.state.docExplorerWidth < 200 ? 'doc-explorer-narrow' : '',
+    ].join(' ');
 
     const variableOpen = this.state.variableEditorOpen;
     const variableStyle = {
@@ -357,7 +361,7 @@ export class GraphiQL extends React.Component {
             </div>
           </div>
         </div>
-        <div className="docExplorerWrap" style={docWrapStyle}>
+        <div className={docExplorerWrapClasses} style={docWrapStyle}>
           <div
             className="docExplorerResizer"
             onMouseDown={this.handleDocsResizeStart}
