dc4a561f796258c6e6773612279abccb76f10ee0
Lee Byron <lee@leebyron.com>
2017-01-09 12:24:13 -0800

    Update copyright/license headers

--- src/utility/__tests__/getQueryFacts-test.js
+++ src/utility/__tests__/getQueryFacts-test.js
@@ -1,9 +1,9 @@
 /**
- *  Copyright (c) 2015, Facebook, Inc.
+ *  Copyright (c) Facebook, Inc.
  *  All rights reserved.
  *
  *  This source code is licensed under the license found in the
- *  LICENSE-examples file in the root directory of this source tree.
+ *  LICENSE file in the root directory of this source tree.
  */
 
 import { expect } from 'chai';
--- src/utility/find.js
+++ src/utility/find.js
@@ -1,12 +1,11 @@
 /* @flow */
 /* eslint-disable no-undef */
 /**
- *  Copyright (c) 2015, Facebook, Inc.
+ *  Copyright (c) Facebook, Inc.
  *  All rights reserved.
  *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant
- *  of patent rights can be found in the PATENTS file in the same directory.
+ *  This source code is licensed under the license found in the
+ *  LICENSE file in the root directory of this source tree.
  */
 
 export default function find<T>(
