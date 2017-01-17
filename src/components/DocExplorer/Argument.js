3b595af1dbdfe30402058bec8bd4b4dccb2057b6
Muriel Silveira <murielkong@gmail.com>
2017-01-17 12:44:41 +0100

    Add copyright at the new file

--- src/components/DocExplorer/Argument.js
+++ src/components/DocExplorer/Argument.js
@@ -1,3 +1,11 @@
+/**
+ *  Copyright (c) Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the license found in the
+ *  LICENSE file in the root directory of this source tree.
+ */
+
 import React, { PropTypes } from 'react';
 import TypeLink from './TypeLink';
 
