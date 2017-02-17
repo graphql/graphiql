b5f2586d03159285af2850be138a15595a250cd4
Greg Hurrell <greg@hurrell.net>
2017-02-17 14:40:33 -0800

    Remove unused chai-json-equal and chai-subset packages
    These were likely only in here because of copy-pasta, so out they go.

--- resources/mocha-bootload.js
+++ resources/mocha-bootload.js
@@ -7,11 +7,6 @@
  */
 /* eslint-disable no-console, object-shorthand */
 
-var chai = require('chai');
-
-var chaiSubset = require('chai-subset');
-chai.use(chaiSubset);
-
 process.on('unhandledRejection', function (error) {
   console.error('Unhandled Promise Rejection:');
   console.error(error && error.stack || error);
