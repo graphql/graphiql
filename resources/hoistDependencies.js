b68863a8ae9e9f4ea8ea7849ce674a439399ac12
Greg Hurrell <greg@hurrell.net>
2017-03-30 08:59:44 -0700

    Replace `npm link` usage with manual linking
    I suspect the links here may be confusing Flow (because they create
    links to absolute paths outside the project root) so I am replacing them
    with simple manual symlinks.
    
    Note that if you just make the links and then run `npm install`, NPM
    will not install any transitive dependencies because it figures they've
    already been installed. So, we mutate the package.json (ugh) by hoisting
    the dependencies up, then we link, and then we let Travis `npm install`.
    
    With this, all tests pass locally for me, but I still have to sort out
    the Flow issues.

---
+++ resources/hoistDependencies.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+/**
+ *  Copyright (c) Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the license found in the
+ *  LICENSE file in the root directory of this source tree.
+ */
+
+/**
+ * Hoists dependencies to the top-level (primarily intended for use in
+ * Travis CI).
+ */
+
+const {readdirSync, writeFileSync} = require('fs');
+const {join} = require('path');
+
+const mainPackage = require('../package.json');
+const otherPackages = readdirSync('packages').map(pkg =>
+  require(join(process.cwd(), 'packages', pkg, 'package.json')));
+
+const dependencies = mainPackage.dependencies;
+otherPackages.forEach(pkg => {
+  if (pkg.dependencies) {
+    Object.keys(pkg.dependencies).forEach(dependency => {
+      const version = pkg.dependencies[dependency];
+      if (dependencies[dependency] && dependencies[dependency] !== version) {
+        const message = `Multiple versions ` +
+          `(${version}, ${dependencies[dependency]}) present for package: ` +
+          dependency;
+        throw new Error(message);
+      }
+      dependencies[dependency] = version;
+    });
+  }
+});
+
+writeFileSync(
+  join(process.cwd(), 'package.json'),
+  JSON.stringify(mainPackage, null, 2) + '\n'
+);
