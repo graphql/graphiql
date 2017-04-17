5128b7041fc964e62b013b7ece1e1919e08038c7
hypnos <Filipp.Riabchun@jetbrains.com>
2017-04-17 05:56:16 +0300

    Replace React.PropTypes with prop-types package

--- src/components/ResultViewer.js
+++ src/components/ResultViewer.js
@@ -6,7 +6,8 @@
  *  LICENSE file in the root directory of this source tree.
  */
 
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 /**
  * ResultViewer
