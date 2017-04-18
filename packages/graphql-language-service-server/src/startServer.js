71baf3da4487e2c7e9e825706bde888a6d3fe5d4
Hyohyeon Jeong <asiandrummer@fb.com>
2017-04-17 23:09:00 -0700

    fix to close the process on socket end, and bump versions for publish

--- packages/graphql-language-service-server/src/startServer.js
+++ packages/graphql-language-service-server/src/startServer.js
@@ -37,7 +37,9 @@ export default (async function startServer(
     const messageReader = new SocketMessageReader(socket);
     const messageWriter = new SocketMessageWriter(socket);
 
-    socket.on('close', () => process.exit(0));
+    socket.on('end', () => {
+      process.exit(0);
+    });
 
     messageReader.listen(message => {
       try {
