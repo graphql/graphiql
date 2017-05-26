3bdb2ac46dfe6ddd97fd20a1e658596afc081239
Greg Hurrell <greg@hurrell.net>
2017-05-26 15:37:49 -0700

    Add missing headers to normalizeWhitespace tests

--- src/utility/__tests__/normalizeWhitespace-test.js
+++ src/utility/__tests__/normalizeWhitespace-test.js
@@ -1,3 +1,10 @@
+/**
+ *  Copyright (c) Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the license found in the
+ *  LICENSE file in the root directory of this source tree.
+ */
 
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
