1a5cbcb4c787eb30500aac556a9fa57c1c468f39
Hyo Jeong <asiandrummer@fb.com>
2017-08-18 11:41:08 -0700

    fix to not use the glob pattern for watchman

--- packages/server/src/GraphQLCache.js
+++ packages/server/src/GraphQLCache.js
@@ -159,7 +159,7 @@ export class GraphQLCache {
       return this._fragmentDefinitionsCache.get(rootDir) || new Map();
     }
 
-    const includes = projectConfig.includes;
+    const includes = projectConfig.includes.map(path => path.split('*')[0]);
     const filesFromInputDirs = await this._watchmanClient.listFiles(rootDir, {
       path: includes,
     });
