09ce2b9755ace912eeb22835f41a46f44bdc199a
Dirk-Jan Rutten <dirkjanrutten@gmail.com>
2017-09-30 15:44:00 +0200

    Initial POC for alphabetic sorting of the info exposed in the docs.

---
+++ src/utility/sortAlphabetically.js
@@ -0,0 +1,21 @@
+/**
+ *  Copyright (c) Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the license found in the
+ *  LICENSE file in the root directory of this source tree.
+ */
+
+export default function(objects) {
+  const sortedObject = objects.concat();
+  sortedObject.sort((a, b) => {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  });
+  return sortedObject;
+}
