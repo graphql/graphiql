dde95d90350d9d6b27f65a005581d45296cc8c51
Matt Gadda <mgadda@twitter.com>
2018-04-10 20:18:37 -0700

    Fixes #221 and #222 - fixes GraphQLCache file globbing

--- packages/server/src/GraphQLCache.js
+++ packages/server/src/GraphQLCache.js
@@ -158,13 +158,9 @@ export class GraphQLCache implements GraphQLCacheInterface {
       return this._fragmentDefinitionsCache.get(rootDir) || new Map();
     }
 
-    const includes = projectConfig.includes.map(
-      filePath => filePath.split('*')[0],
-    );
-
     const filesFromInputDirs = await this._readFilesFromInputDirs(
       rootDir,
-      includes,
+      projectConfig.includes,
     );
     const list = filesFromInputDirs.filter(fileInfo =>
       projectConfig.includesFile(fileInfo.filePath),
@@ -246,9 +242,15 @@ export class GraphQLCache implements GraphQLCacheInterface {
     rootDir: string,
     includes: string[],
   ): Promise<Array<GraphQLFileMetadata>> => {
+    let pattern: string;
+    if (includes.length === 1) {
+      pattern = includes[0];
+    } else {
+      pattern = `{${includes.join(',')}}`;
+    }
     return new Promise((resolve, reject) => {
       const globResult = new glob.Glob(
-        `{${includes.join(',')}}/**/*.{js,graphql}`,
+        pattern,
         {
           cwd: rootDir,
           stat: true,
