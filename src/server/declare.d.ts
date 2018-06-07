42bb8b20ea7680c20b51cdaf653132f1035269c2
Divyendu Singh <divyendu.z@gmail.com>
2018-06-07 23:27:40 +0200

    extensions are optional

--- src/server/declare.d.ts
+++ src/server/declare.d.ts
@@ -1,7 +1,7 @@
 declare module "graphql-language-service-server" {
   interface Options {
     method?: "socket" | "stream" | "node";
-    extensions: Array<any>;
+    extensions?: Array<any>;
   }
 
   function startServer(options: Options): Promise<void>;
