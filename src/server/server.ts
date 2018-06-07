02d68f6fad671f5e6c0db4f1b8b9de43fc192177
Divyendu Singh <divyendu.z@gmail.com>
2018-06-07 15:15:37 +0200

    feature prisma directives via extension

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,13 @@
 import { startServer } from "graphql-language-service-server";
 import "babel-polyfill";
+import { patchConfig } from "graphql-config-extension-prisma";
 
 (async () => {
   try {
-    await startServer({ method: "node" });
+    await startServer({
+      method: "node",
+      extensions: [patchConfig]
+    });
   } catch (err) {
     console.error(err);
   }
