272e2371fc7715217739efd7817ce6343cb4fbec
Angel Gomez Salazar <agomezs@fb.com>
2018-09-07 14:20:58 -0700

    Change License to MIT

--- resources/mocha-bootload.js
+++ resources/mocha-bootload.js
@@ -1,9 +1,8 @@
 /* eslint-disable no-console, object-shorthand */
 /**
- *  Copyright (c) Facebook, Inc.
- *  All rights reserved.
+ *  Copyright (c) Facebook, Inc. and its affiliates.
  *
- *  This source code is licensed under the license found in the
+ *  This source code is licensed under the MIT license found in the
  *  LICENSE file in the root directory of this source tree.
  */
 
