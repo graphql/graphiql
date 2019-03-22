ac274d3fb81d4e580920357f0563223c87246d23
Jordan Eldredge <jordan@jordaneldredge.com>
2019-03-22 15:28:51 -0700

    Capture Cmd/Ctrl-G (#774)
    My usage of search in Chrome/Firefox is generally:
    
    1. Cmd-F to open the search input
    2. Type my query
    3. Type Cmd-G to iterate through the search results
    
    When I do this out of muscle memory in GraphQL, I get suck since the
    `Cmd-G` gets captured by the browser and puts my focus in the browsers
    search input.
    
    With this fix, my normal flow works as expected.

--- src/components/ResultViewer.js
+++ src/components/ResultViewer.js
@@ -72,6 +72,8 @@ export class ResultViewer extends React.Component {
         // Persistent search box in Query Editor
         'Cmd-F': 'findPersistent',
         'Ctrl-F': 'findPersistent',
+        'Cmd-G': 'findPersistent',
+        'Ctrl-G': 'findPersistent',
 
         // Editor improvements
         'Ctrl-Left': 'goSubwordLeft',
