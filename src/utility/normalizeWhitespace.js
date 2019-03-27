7766eadbc33d082528ed17c15a72eee43828628f
Osipov Vladimir <zouxuoz@gmail.com>
2019-03-27 14:34:39 +0300

    Normalizes no-break spaces

--- src/utility/normalizeWhitespace.js
+++ src/utility/normalizeWhitespace.js
@@ -9,7 +9,7 @@
 export const invalidCharacters = Array.from({ length: 11 }, (x, i) => {
   // \u2000 -> \u200a
   return String.fromCharCode(0x2000 + i);
-}).concat(['\u2028', '\u2029', '\u202f']);
+}).concat(['\u2028', '\u2029', '\u202f', '\u00a0']);
 
 const sanitizeRegex = new RegExp('[' + invalidCharacters.join('') + ']', 'g');
 
