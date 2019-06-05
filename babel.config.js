be1e5fc718a3b8278d25560bfac37b78306fe52b
Rikki Schulte <richard.j.schulte@gmail.com>
2019-06-05 01:34:44 -0400

    import graphql-language-service into graphiql - rename paths to match their package names - remove unused tooling from early monorepo

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  presets: [require.resolve("@babel/preset-env"), require.resolve("@babel/preset-flow")],
+  presets: [
+    require.resolve("@babel/preset-env"), 
+    require.resolve("@babel/preset-flow")
+  ],
   plugins: [
-    require.resolve("@babel/plugin-proposal-class-properties"),
-    require.resolve("@babel/plugin-transform-modules-commonjs")
+    require.resolve("@babel/plugin-proposal-class-properties")
   ]
 }
