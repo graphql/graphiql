1ecd2045ad62c32c6048c5217990d746e2a13060
Divyendu Singh <singh@prisma.io>
2019-06-24 17:32:53 +0200

    use graphql-language-service-server from upstream

--- src/server/declare.d.ts
+++ src/server/declare.d.ts
@@ -1,8 +1,8 @@
-declare module "@divyenduz/graphql-language-service-server" {
+declare module "graphql-language-service-server" {
   interface Options {
-    method?: "socket" | "stream" | "node";
-    extensions?: Array<any>;
+    method?: "socket" | "stream" | "node"
+    extensions?: Array<any>
   }
 
-  function startServer(options: Options): Promise<void>;
+  function startServer(options: Options): Promise<void>
 }
