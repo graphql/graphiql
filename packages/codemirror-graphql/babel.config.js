11bb21afd7a85b8139f324d72d9c2c820b67a476
Giancarlo Anemone <ganemone@uber.com>
2019-06-25 11:18:37 -0400

    Add babel config for codemirror-graphql

---
+++ packages/codemirror-graphql/babel.config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  presets: [
+    require.resolve('@babel/preset-env'),
+    require.resolve('@babel/preset-flow'),
+    require.resolve('@babel/preset-react'),
+  ],
+  plugins: [require.resolve('@babel/plugin-proposal-class-properties')],
+};
