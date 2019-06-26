ff1f0fba15bcbad806135a39ccea734bb8b37be7
Rikki Schulte <richard.j.schulte@gmail.com>
2019-06-25 23:32:12 -0400

    move examples, build as subpackage, more version bumps

--- packages/graphiql/babel.config.js
+++ packages/graphiql/babel.config.js
@@ -3,24 +3,32 @@ const config = {
     require.resolve('@babel/preset-flow'),
     require.resolve('@babel/preset-react'),
   ],
-  plugins: [
-    require.resolve('@babel/plugin-proposal-class-properties')
-  ],
+  plugins: [require.resolve('@babel/plugin-proposal-class-properties')],
 };
 
 if (process.env.CDN) {
-  config.presets.push(require.resolve('@babel/preset-env'));
-} else {
-  config.presets.push([
-    require.resolve('@babel/preset-env'),
-    {
-      modules: 'commonjs',
-      ignoreBrowserslistConfig: true,
-      targets: {
-        browsers: 'last 2 versions',
+  config.presets.push(
+    [
+      require.resolve('@babel/preset-env'),
+      { 
+        useBuiltIns: 'entry'
       },
-    },
-  ]);
+    ]
+  );
+} else {
+  config.presets.push(
+    [
+      require.resolve('@babel/preset-env'),
+      {
+        useBuiltIns: 'entry',
+        modules: 'commonjs',
+        ignoreBrowserslistConfig: true,
+        targets: {
+          browsers: 'last 2 versions',
+        },
+      },
+    ]
+  );
 }
 
 module.exports = config;
--- packages/graphiql/resources/build.sh
+++ packages/graphiql/resources/build.sh
@@ -15,12 +15,10 @@ babel src --ignore __tests__ --out-dir dist/
 CDN=true babel src --ignore __tests__ --out-dir cdn/
 
 echo "Bundling graphiql.js..."
-browserify -g browserify-shim -r regenerator-runtime/runtime -s GraphiQL cdn/index.js > graphiql.js
+browserify -g browserify-shim -s GraphiQL cdn/index.js > graphiql.js
 
 echo "Bundling graphiql.min.js..."
-browserify -g browserify-shim \
--r regenerator-runtime/runtime -t uglifyify -s GraphiQL \
-cdn/index.js 2> /dev/null | uglifyjs -c > graphiql.min.js 2> /dev/null
+browserify -g browserify-shim -t uglifyify -s GraphiQL cdn/index.js 2> /dev/null | uglifyjs -c > graphiql.min.js 2> /dev/null
 
 echo "Bundling graphiql.css..."
 postcss --no-map --use autoprefixer -d dist/ css/*.css
