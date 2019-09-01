11b5726855695399411934f3bb2f43433841fd6b
Rikki Schulte <richard.j.schulte@gmail.com>
2019-09-01 14:26:24 -0400

    chore: more prep for devrelease

--- resources/prerelease.js
+++ resources/prerelease.js
@@ -1,42 +1,49 @@
-const execa = require('execa');
+const execFile = require('child_process').execFile;
+const { promisify } = require('util');
 const argv = require('yargs').argv;
 
+const execFileAsync = promisify(execFile);
 /**
  *
  * @param {yargs<process.argv>} args from yargs
  */
 const conventionalPrerelease = async args => {
-  try {
-    const { _, $0, packages, ...otherArgs } = args;
-    if (!packages || !packages.length) {
-      throw Error(
-        `must supply commas seperated list of packages to apply prerelease for,\n ala --packages "'package-name','package-name'",\n or supply --packages all for all packages`
-      );
-      process.exit(1);
-    }
-    let finalArgs = [
-      'version',
-      '--conventional-commits',
-      '--conventional-prerelease',
-      '--allow-branch',
-      '*',
-    ];
-    if (otherArgs) {
-      finalArgs = [...finalArgs, ...process.argv.slice(finalArgs.length - 1)];
-      console.log('finalArgs', finalArgs);
-    }
-    // defualt lerna.json configs supply other important arguments here
-    await execa('lerna', [...finalArgs, packages === 'all' ? '*' : packages]);
-    console.log(
-      "'yarn prerelease' has completed, you can now 'yarn graduate'\n these packages to full release versions only from a master branch"
+  const { _, $0, packages, ...otherArgs } = args;
+  if (!packages || !packages.length) {
+    throw Error(
+      `must supply commas seperated list of packages to apply prerelease for,\n ala --packages "'package-name','package-name'",\n or supply --packages all for all packages`,
     );
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
   }
+  let finalArgs = [
+    'version',
+    '--conventional-commits',
+    '--conventional-prerelease',
+    '--allow-branch',
+    '*',
+  ];
+  if (otherArgs) {
+    finalArgs = [...finalArgs, ...process.argv.slice(finalArgs.length - 1)];
+  }
+
+  // defualt lerna.json configs supply other important arguments here
+  const result = await execFileAsync('lerna', [
+    ...finalArgs,
+    packages === 'all' ? '*' : packages,
+  ]);
+  console.log('result', result);
+  console.log(
+    "'yarn prerelease' has completed, you can now 'yarn graduate'\n these packages to full release versions only from a master branch",
+  );
+  process.exit(0);
 };
 
 (async () => {
-  console.log(argv);
-  await conventionalPrerelease(argv);
+  try {
+    await conventionalPrerelease(argv);
+  } catch (err) {
+    console.error(
+      err.message.includes('\n') ? err.message.split('\n')[0] : err.message,
+    );
+    process.exit(1);
+  }
 })();
