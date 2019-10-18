7000d5072c6865d32f36c355c3071024ebaaad11
Samuel <samuelimolo4real@gmail.com>
2019-10-19 00:17:13 +0200

    Changed source minify to graphiql.js
    The source for the minify operation should be graphiql.js for it to work properly.

--- packages/graphiql/resources/build.sh
+++ packages/graphiql/resources/build.sh
@@ -13,7 +13,7 @@ babel src --ignore __tests__ --out-dir dist/
 echo "Bundling graphiql.js..."
 browserify -g browserify-shim -s GraphiQL dist/index.js > graphiql.js
 echo "Bundling graphiql.min.js..."
-browserify -g browserify-shim -t uglifyify -s GraphiQL graphiql.min.js | uglifyjs -c > graphiql.min.js
+browserify -g browserify-shim -t uglifyify -s GraphiQL graphiql.js | uglifyjs -c > graphiql.min.js
 echo "Bundling graphiql.css..."
 postcss --no-map --use autoprefixer -d dist/ css/*.css
 cat dist/*.css > graphiql.css
