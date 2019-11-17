a3f4d1a295034ddd6d9e07269a70d94452f225b9
Rikki Schulte <richard.j.schulte@gmail.com>
2019-11-17 11:38:16 -0500

    chore: esm for all packages

--- packages/graphiql/babel.config.js
+++ packages/graphiql/babel.config.js
@@ -1,13 +1,1 @@
-module.exports = {
-  presets: [
-    [
-      require.resolve('@babel/preset-env'),
-      {
-        targets: '> 0.25%, not dead',
-      },
-    ],
-    require.resolve('@babel/preset-flow'),
-    require.resolve('@babel/preset-react'),
-  ],
-  plugins: [require.resolve('@babel/plugin-proposal-class-properties')],
-};
+module.exports = require('../../babel.config')
