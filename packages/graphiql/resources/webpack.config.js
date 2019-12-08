3b1c2c142221b1a101c5d378242059dbcaeb9eab
Rikki Schulte <richard.j.schulte@gmail.com>
2019-12-08 18:34:35 -0500

    fix: test output, webpack resolution, clean build
    - remove build test output for ts
    - actual clean build (use rimraf not rm -rf)
    - remove css bundling workaround, use seperate entrypoint for webpack bundle vs css

--- packages/graphiql/resources/webpack.config.js
+++ packages/graphiql/resources/webpack.config.js
@@ -17,9 +17,9 @@ const resultConfig = {
         'react-hot-loader/patch', // activate HMR for React
         'webpack-dev-server/client?http://localhost:8080', // bundle the client for webpack-dev-server and connect to the provided endpoint
         'webpack/hot/only-dev-server', // bundle the client for hot reloading, only- means to only hot reload for successful updates
-        './index.js', // the entry point of our app
+        './cdn.js', // the entry point of our app
       ]
-    : './index.js',
+    : './cdn.js',
   context: rootPath('src'),
   output: {
     path: rootPath(),
