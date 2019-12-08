38da32d059cf54784af3c0767d7a92b1ba94d9b7
Rikki Schulte <richard.j.schulte@gmail.com>
2019-12-08 18:34:35 -0500

    fix: test output, webpack resolution, clean build
    - remove build test output for ts
    - actual clean build (use rimraf not rm -rf)
    - remove css bundling workaround, use seperate entrypoint for webpack bundle vs css

--- packages/graphiql/src/cdn.js
+++ packages/graphiql/src/cdn.js
@@ -6,5 +6,17 @@
  */
 import 'regenerator-runtime/runtime';
 
+import './css/app.css';
+import './css/codemirror.css';
+import './css/foldgutter.css';
+import './css/info.css';
+import './css/jump.css';
+import './css/lint.css';
+import './css/loading.css';
+import './css/show-hint.css';
+
+import './css/doc-explorer.css';
+import './css/history.css';
+
 import { GraphiQL } from './components/GraphiQL';
 export default GraphiQL;
