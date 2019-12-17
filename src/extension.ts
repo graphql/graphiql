7d31c00010467889f3f05c79cfae4dcc7bf62ba2
Divyendu Singh <singh@prisma.io>
2019-12-17 16:40:37 +0100

    bump deps

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
-"use strict";
-import * as path from "path";
-import * as dotenv from "dotenv";
-import * as fs from "fs";
+"use strict"
+import * as path from "path"
+import * as dotenv from "dotenv"
+import * as fs from "fs"
 import {
   workspace,
   ExtensionContext,
@@ -10,80 +10,80 @@ import {
   OutputChannel,
   languages,
   Uri,
-  ViewColumn
-} from "vscode";
+  ViewColumn,
+} from "vscode"
 import {
   LanguageClient,
   LanguageClientOptions,
   ServerOptions,
-  TransportKind
-} from "vscode-languageclient";
+  TransportKind,
+} from "vscode-languageclient"
 
-import statusBarItem, { initStatusBar } from "./status";
+import statusBarItem, { initStatusBar } from "./status"
 
-import { GraphQLContentProvider } from "./client/graphql-content-provider";
-import { GraphQLCodeLensProvider } from "./client/graphql-codelens-provider";
-import { ExtractedTemplateLiteral } from "./client/source-helper";
-import { CustomInitializationFailedHandler } from "./CustomInitializationFailedHandler";
+import { GraphQLContentProvider } from "./client/graphql-content-provider"
+import { GraphQLCodeLensProvider } from "./client/graphql-codelens-provider"
+import { ExtractedTemplateLiteral } from "./client/source-helper"
+import { CustomInitializationFailedHandler } from "./CustomInitializationFailedHandler"
 
 function getConfig() {
   return workspace.getConfiguration(
     "vscode-graphql",
-    window.activeTextEditor ? window.activeTextEditor.document.uri : null
-  );
+    window.activeTextEditor ? window.activeTextEditor.document.uri : null,
+  )
 }
 
 function getEnvironment() {
   if (workspace.workspaceFolders === undefined) {
-    return process.env;
+    return process.env
   }
 
-  let workspaceEnv = {};
+  let workspaceEnv = {}
   workspace.workspaceFolders.forEach(folder => {
-    const envPath = `${folder.uri.fsPath}/.env`;
+    const envPath = `${folder.uri.fsPath}/.env`
     if (fs.existsSync(envPath)) {
       workspaceEnv = {
         ...workspaceEnv,
-        ...dotenv.parse(fs.readFileSync(envPath))
-      };
+        ...dotenv.parse(fs.readFileSync(envPath)),
+      }
     }
-  });
+  })
 
-  return { ...workspaceEnv, ...process.env };
+  return { ...workspaceEnv, ...process.env }
 }
 
 export async function activate(context: ExtensionContext) {
   let outputChannel: OutputChannel = window.createOutputChannel(
-    "GraphQL Language Server"
-  );
-  const config = getConfig();
-  const { debug } = config;
+    "GraphQL Language Server",
+  )
+  const config = getConfig()
+  const { debug } = config
   if (debug) {
-    console.log('Extension "vscode-graphql" is now active!');
+    console.log('Extension "vscode-graphql" is now active!')
   }
 
   const serverModule = context.asAbsolutePath(
-    path.join("out/server", "server.js")
-  );
+    path.join("out/server", "server.js"),
+  )
 
   const debugOptions = {
-    execArgv: ["--nolazy", "--debug=6009", "--inspect=localhost:6009"]
-  };
+    execArgv: ["--nolazy", "--debug=6009", "--inspect=localhost:6009"],
+  }
 
-  const combinedEnv = getEnvironment();
+  const combinedEnv = getEnvironment()
 
   let serverOptions: ServerOptions = {
     run: {
       module: serverModule,
       transport: TransportKind.ipc,
-      options: { env: combinedEnv }
+      options: { env: combinedEnv },
     },
     debug: {
       module: serverModule,
       transport: TransportKind.ipc,
-      options: { ...(debug ? debugOptions : {}), env: combinedEnv }
-    }
-  };
+      options: { ...(debug ? debugOptions : {}), env: combinedEnv },
+    },
+  }
 
   let clientOptions: LanguageClientOptions = {
     documentSelector: [
@@ -91,39 +91,39 @@ export async function activate(context: ExtensionContext) {
       { scheme: "file", language: "javascript" },
     ],
     synchronize: {
-      fileEvents: workspace.createFileSystemWatcher("**/*.{graphql,gql,js}")
+      fileEvents: workspace.createFileSystemWatcher("**/*.{graphql,gql,js}"),
     },
     outputChannel: outputChannel,
     outputChannelName: "GraphQL Language Server",
     initializationFailedHandler: CustomInitializationFailedHandler(
-      outputChannel
-    )
-  };
+      outputChannel,
+    ),
+  }
 
   const client = new LanguageClient(
     "vscode-graphql",
     "GraphQL Language Server",
     serverOptions,
     clientOptions,
-    debug
-  );
+    debug,
+  )
 
-  const disposableClient = client.start();
-  context.subscriptions.push(disposableClient);
+  const disposableClient = client.start()
+  context.subscriptions.push(disposableClient)
 
   const commandIsDebugging = commands.registerCommand(
     "extension.isDebugging",
     () => {
-      outputChannel.appendLine(`is in debug mode: ${!!debug}`);
-    }
-  );
-  context.subscriptions.push(commandIsDebugging);
+      outputChannel.appendLine(`is in debug mode: ${!!debug}`)
+    },
+  )
+  context.subscriptions.push(commandIsDebugging)
 
   // Manage Status Bar
-  context.subscriptions.push(statusBarItem);
+  context.subscriptions.push(statusBarItem)
   client.onReady().then(() => {
-    initStatusBar(statusBarItem, client, window.activeTextEditor);
-  });
+    initStatusBar(statusBarItem, client, window.activeTextEditor)
+  })
 
   context.subscriptions.push(
     languages.registerCodeLensProvider(
@@ -132,44 +132,44 @@ export async function activate(context: ExtensionContext) {
         "typescript",
         "javascriptreact",
         "typescriptreact",
-        "graphql"
+        "graphql",
       ],
-      new GraphQLCodeLensProvider(outputChannel)
-    )
-  );
+      new GraphQLCodeLensProvider(outputChannel),
+    ),
+  )
 
   const commandContentProvider = commands.registerCommand(
     "extension.contentProvider",
     async (literal: ExtractedTemplateLiteral) => {
-      const uri = Uri.parse("graphql://authority/graphql");
+      const uri = Uri.parse("graphql://authority/graphql")
 
       const panel = window.createWebviewPanel(
         "executionReusltWebView",
         "GraphQL Execution Result",
         ViewColumn.Two,
-        {}
-      );
+        {},
+      )
 
       const contentProvider = new GraphQLContentProvider(
         uri,
         outputChannel,
         literal,
         panel,
-        combinedEnv
-      );
+        combinedEnv,
+      )
       const registration = workspace.registerTextDocumentContentProvider(
         "graphql",
-        contentProvider
-      );
-      context.subscriptions.push(registration);
+        contentProvider,
+      )
+      context.subscriptions.push(registration)
 
-      const html = await contentProvider.getCurrentHtml();
-      panel.webview.html = html;
-    }
-  );
-  context.subscriptions.push(commandContentProvider);
+      const html = await contentProvider.getCurrentHtml()
+      panel.webview.html = html
+    },
+  )
+  context.subscriptions.push(commandContentProvider)
 }
 
 export function deactivate() {
-  console.log('Extension "vscode-graphql" is now de-active!');
+  console.log('Extension "vscode-graphql" is now de-active!')
 }
