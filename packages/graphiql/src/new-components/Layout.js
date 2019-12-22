2eb4100bddc64a11048b33bdf155a6a06561593d
Laura González <walaura@fb.com>
2019-12-22 10:56:27 +0000

    add wip stories

---
+++ packages/graphiql/src/new-components/Layout.js
@@ -0,0 +1,43 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
+
+import PropTypes from 'prop-types';
+
+const Widget = ({ children }) => <div sx={{ padding: 2 }}>{children}</div>
+
+const Container = ({ children, mini = false }) => <div sx={
+  {
+    backgroundColor: !mini && 'cardBackground',
+    boxShadow: !mini && (theme => `0 0 0 .1px ${theme.colors.border}, 0 1px 4px 0 ${theme.colors.border}`)
+  }}>
+  {children}
+</div>
+
+Container.propTypes = {
+  mini: PropTypes.bool
+}
+
+const Layout = ({ nav = 'nav' }) => {
+  return (
+    <main
+      sx={{
+        display: 'grid',
+        gap: 2,
+        padding: 2,
+        gridAutoFlow: 'column',
+        gridTemplateColumns: '6em',
+        gridAutoColumns: 'minmax(30em, 1fr)',
+        minHeight: '100vh',
+      }}>
+      <Container mini>{nav}</Container>
+      <Container><Widget>{'container'}</Widget></Container>
+      <Container><Widget>{'container'}</Widget></Container>
+      <Container><Widget>{'container'}</Widget></Container>
+    </main>
+  );
+};
+
+Layout.propTypes = {
+  nav: PropTypes.node,
+};
+export default Layout;
---
+++ packages/graphiql/src/new-components/Nav.js
@@ -0,0 +1,38 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
+
+const Item = ({ children }) => <button sx={
+    {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        cursor: 'pointer',
+        transition: '.2s',
+        ':hover': {
+            transform: 'scale(1.1)'
+        },
+        ':active': {
+            transform: 'scale(.95)'
+        }
+    }}>
+    {children}
+</button>
+
+
+const Nav = () => {
+    return (
+        <nav
+            sx={{
+                display: 'grid',
+                gridAutoFlow: 'row',
+                gridAutoRows: '2em',
+                fontSize: '3em'
+            }}>
+            <Item>{'🐽'}</Item>
+            <Item>{'👨‍🌾'}</Item>
+            <Item>{'🐝'}</Item>
+        </nav>
+    );
+};
+
+export default Nav;
