bcb27538d9074afdac3ff56074046965b456e57a
Rikki Schulte <rikki.schulte@gmail.com>
2020-01-20 23:24:59 -0500

    chore: improve CI scripts, fix crosswise test resolution, cleanup (#1245)
    * chore: improve CI time, fix crosswise test resolution, cleanup
    
    - fix jest issue w/ moduleNameMapper, so tests don't need a build to run
    - reconfigured build tooling for more streamlined ci. fails more quickly.
    - test = jest now
    - remove most scripts from subpackages, almost everything should be done from root now
    - use browserslist for esm exports

--- jest.config.js
+++ jest.config.js
@@ -20,6 +20,10 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       'identity-obj-proxy',
     '\\.(css|less)$': 'identity-obj-proxy',
+    '^graphql-language-([^/]+)': '<rootDir>/packages/graphql-language-$1/src',
+    '^codemirror-graphql\\/([^]+)':
+      '<rootDir>/packages/codemirror-graphql/src/$1',
+    '^example-([^/]+)': '<rootDir>/examples/$1/src',
   },
   transform: {
     '^.+\\.jsx?$': require.resolve('./resources/jestBabelTransform'),
