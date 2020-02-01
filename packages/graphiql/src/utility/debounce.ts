78edc23fc6181342b72895233083375625430f8f
Rohit Gohri <gohri.rohit@gmail.com>
2020-02-01 20:46:47 +0530

    chore(types): convert debounce to generic, add explicit any

--- packages/graphiql/src/utility/debounce.ts
+++ packages/graphiql/src/utility/debounce.ts
@@ -9,15 +9,18 @@
  * Provided a duration and a function, returns a new function which is called
  * `duration` milliseconds after the last call.
  */
-export default function debounce(duration: number, fn: Function) {
+export default function debounce<F extends (...args: any[]) => any>(
+  duration: number,
+  fn: F,
+) {
   let timeout: number | null;
-  return function() {
+  return function(this: any, ...args: Parameters<F>) {
     if (timeout) {
       window.clearTimeout(timeout);
     }
     timeout = window.setTimeout(() => {
       timeout = null;
-      fn.apply(this, arguments);
+      fn.apply(this, args);
     }, duration);
   };
 }
