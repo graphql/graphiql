b015721b9ec022d60046f7766b33e6025ba58aa3
Arik Cohen <arik@say.co>
2020-02-14 09:37:54 -0500

    created express app

---
+++ examples/graphiql-docker/app.js
@@ -0,0 +1,23 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', function(req, res, next) {
+  res.render('index', { GRAPHQL_API: process.env.GRAPHQL_API || 'http://localhost:8080/graphiql' });
+});
+
+module.exports = app;
