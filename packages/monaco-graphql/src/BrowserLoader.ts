0f5b79eea921f4c6799abae3a22c907907d513c3
Rikki Schulte <rikki.schulte@gmail.com>
2020-02-28 13:17:18 -0500

    feat: set up monaco service

--- packages/monaco-graphql/src/BrowserLoader.ts
+++ packages/monaco-graphql/src/BrowserLoader.ts
@@ -1,35 +1,53 @@
-import { Source, parseGraphQLJSON, SchemaPointerSingle, DocumentLoader, isValidPath, SingleFileOptions } from '@graphql-toolkit/common';
+import {
+  Source,
+  parseGraphQLJSON,
+  SchemaPointerSingle,
+  DocumentLoader,
+  isValidPath,
+  SingleFileOptions,
+} from '@graphql-toolkit/common';
 
-import { StorageAPI } from './StorageAPI'
+import { StorageAPI } from './StorageAPI';
 
 type BrowserLoaderOptions = {
-    path: string
-} & SingleFileOptions
+  path: string;
+} & SingleFileOptions;
 
-const storage = new StorageAPI()
+const storage = new StorageAPI();
 
 export class BrowserLoader implements DocumentLoader {
-    loaderId(): string {
-        return 'browser';
+  loaderId(): string {
+    return 'browser';
+  }
+
+  async canLoad(
+    _pointer: SchemaPointerSingle,
+    options: BrowserLoaderOptions,
+  ): Promise<boolean> {
+    return Boolean(storage.get(options.path));
+  }
+
+  async load(
+    pointer: SchemaPointerSingle,
+    options: BrowserLoaderOptions,
+  ): Promise<Source> {
+    if (!options.path) {
+      throw Error('path option not provided');
     }
 
-    async canLoad(_pointer: SchemaPointerSingle, options: BrowserLoaderOptions): Promise<boolean> {
-        return Boolean(storage.get(options.path))
+    try {
+      const jsonContent = await storage.get<string>(options.path);
+      if (!jsonContent) {
+        await storage.set(
+          options.path,
+          `{ "schema": "http://localhost:8080/graphql" }`,
+        );
+      }
+      return parseGraphQLJSON(pointer, jsonContent, options);
+    } catch (e) {
+      throw new Error(
+        `Unable to read browser json: ${options.path}: ${e.message || e}`,
+      );
     }
-
-    async load(pointer: SchemaPointerSingle, options: BrowserLoaderOptions): Promise<Source> {
-        if (!options.path) {
-            throw Error('path option not provided')
-        }
-
-        try {
-            const jsonContent = await storage.get<string>(options.path)
-            if (!jsonContent) {
-                await storage.set(options.path, `{ "schema": "http://localhost:8080/graphql" }`)
-            }
-            return parseGraphQLJSON(pointer, jsonContent, options);
-        } catch (e) {
-            throw new Error(`Unable to read browser json: ${options.path}: ${e.message || e}`);
-        }
-    }
+  }
 }
--- packages/monaco-graphql/src/graphqlMode.ts
+++ packages/monaco-graphql/src/graphqlMode.ts
@@ -1,4 +1,4 @@
-import * as monaco from 'monaco-editor'
+import * as monaco from 'monaco-editor';
 
 import { WorkerManager } from './workerManager';
 import { JSONWorker } from './graphqlWorker';
@@ -10,99 +10,150 @@ import Uri = monaco.Uri;
 import IDisposable = monaco.IDisposable;
 
 export function setupMode(defaults: LanguageServiceDefaultsImpl): IDisposable {
+  const disposables: IDisposable[] = [];
+  const providers: IDisposable[] = [];
 
-    const disposables: IDisposable[] = [];
-    const providers: IDisposable[] = [];
+  const client = new WorkerManager(defaults);
+  disposables.push(client);
 
-    const client = new WorkerManager(defaults);
-    disposables.push(client);
+  const worker: languageFeatures.WorkerAccessor = (
+    ...uris: Uri[]
+  ): Promise<JSONWorker> => {
+    return client.getLanguageServiceWorker(...uris);
+  };
 
-    const worker: languageFeatures.WorkerAccessor = (...uris: Uri[]): Promise<JSONWorker> => {
-        return client.getLanguageServiceWorker(...uris);
-    };
+  function registerProviders(): void {
+    const { languageId, modeConfiguration } = defaults;
 
+    disposeAll(providers);
 
-    function registerProviders(): void {
-        const { languageId, modeConfiguration } = defaults;
-
-        disposeAll(providers);
-
-        if (modeConfiguration.documentFormattingEdits) {
-            providers.push(monaco.languages.registerDocumentFormattingEditProvider(languageId, new languageFeatures.DocumentFormattingEditProvider(worker)));
-        }
-        if (modeConfiguration.documentRangeFormattingEdits) {
-            providers.push(monaco.languages.registerDocumentRangeFormattingEditProvider(languageId, new languageFeatures.DocumentRangeFormattingEditProvider(worker)));
-        }
-        if (modeConfiguration.completionItems) {
-            providers.push(monaco.languages.registerCompletionItemProvider(languageId, new languageFeatures.CompletionAdapter(worker)));
-        }
-        if (modeConfiguration.hovers) {
-            providers.push(monaco.languages.registerHoverProvider(languageId, new languageFeatures.HoverAdapter(worker)));
-        }
-        if (modeConfiguration.documentSymbols) {
-            providers.push(monaco.languages.registerDocumentSymbolProvider(languageId, new languageFeatures.DocumentSymbolAdapter(worker)));
-        }
-        if (modeConfiguration.tokens) {
-            providers.push(monaco.languages.setTokensProvider(languageId, createTokenizationSupport(true)));
-        }
-        if (modeConfiguration.colors) {
-            providers.push(monaco.languages.registerColorProvider(languageId, new languageFeatures.DocumentColorAdapter(worker)));
-        }
-        if (modeConfiguration.foldingRanges) {
-            providers.push(monaco.languages.registerFoldingRangeProvider(languageId, new languageFeatures.FoldingRangeAdapter(worker)));
-        }
-        if (modeConfiguration.diagnostics) {
-            providers.push(new languageFeatures.DiagnosticsAdapter(languageId, worker, defaults));
-        }
-        if (modeConfiguration.selectionRanges) {
-            providers.push(monaco.languages.registerSelectionRangeProvider(languageId, new languageFeatures.SelectionRangeAdapter(worker)));
-        }
+    if (modeConfiguration.documentFormattingEdits) {
+      providers.push(
+        monaco.languages.registerDocumentFormattingEditProvider(
+          languageId,
+          new languageFeatures.DocumentFormattingEditProvider(worker),
+        ),
+      );
     }
+    if (modeConfiguration.documentRangeFormattingEdits) {
+      providers.push(
+        monaco.languages.registerDocumentRangeFormattingEditProvider(
+          languageId,
+          new languageFeatures.DocumentRangeFormattingEditProvider(worker),
+        ),
+      );
+    }
+    if (modeConfiguration.completionItems) {
+      providers.push(
+        monaco.languages.registerCompletionItemProvider(
+          languageId,
+          new languageFeatures.CompletionAdapter(worker),
+        ),
+      );
+    }
+    if (modeConfiguration.hovers) {
+      providers.push(
+        monaco.languages.registerHoverProvider(
+          languageId,
+          new languageFeatures.HoverAdapter(worker),
+        ),
+      );
+    }
+    if (modeConfiguration.documentSymbols) {
+      providers.push(
+        monaco.languages.registerDocumentSymbolProvider(
+          languageId,
+          new languageFeatures.DocumentSymbolAdapter(worker),
+        ),
+      );
+    }
+    if (modeConfiguration.tokens) {
+      providers.push(
+        monaco.languages.setTokensProvider(
+          languageId,
+          createTokenizationSupport(true),
+        ),
+      );
+    }
+    if (modeConfiguration.colors) {
+      providers.push(
+        monaco.languages.registerColorProvider(
+          languageId,
+          new languageFeatures.DocumentColorAdapter(worker),
+        ),
+      );
+    }
+    if (modeConfiguration.foldingRanges) {
+      providers.push(
+        monaco.languages.registerFoldingRangeProvider(
+          languageId,
+          new languageFeatures.FoldingRangeAdapter(worker),
+        ),
+      );
+    }
+    if (modeConfiguration.diagnostics) {
+      providers.push(
+        new languageFeatures.DiagnosticsAdapter(languageId, worker, defaults),
+      );
+    }
+    if (modeConfiguration.selectionRanges) {
+      providers.push(
+        monaco.languages.registerSelectionRangeProvider(
+          languageId,
+          new languageFeatures.SelectionRangeAdapter(worker),
+        ),
+      );
+    }
+  }
 
-    registerProviders();
+  registerProviders();
 
-    disposables.push(monaco.languages.setLanguageConfiguration(defaults.languageId, richEditConfiguration));
+  disposables.push(
+    monaco.languages.setLanguageConfiguration(
+      defaults.languageId,
+      richEditConfiguration,
+    ),
+  );
 
-    let modeConfiguration = defaults.modeConfiguration;
-    defaults.onDidChange((newDefaults) => {
-        if (newDefaults.modeConfiguration !== modeConfiguration) {
-            modeConfiguration = newDefaults.modeConfiguration;
-            registerProviders();
-        }
-    });
+  let modeConfiguration = defaults.modeConfiguration;
+  defaults.onDidChange(newDefaults => {
+    if (newDefaults.modeConfiguration !== modeConfiguration) {
+      modeConfiguration = newDefaults.modeConfiguration;
+      registerProviders();
+    }
+  });
 
-    disposables.push(asDisposable(providers));
+  disposables.push(asDisposable(providers));
 
-    return asDisposable(disposables);
+  return asDisposable(disposables);
 }
 
 function asDisposable(disposables: IDisposable[]): IDisposable {
-    return { dispose: () => disposeAll(disposables) };
+  return { dispose: () => disposeAll(disposables) };
 }
 
 function disposeAll(disposables: IDisposable[]) {
-    while (disposables.length) {
-        disposables.pop().dispose();
-    }
+  while (disposables.length) {
+    disposables.pop().dispose();
+  }
 }
 
 const richEditConfiguration: monaco.languages.LanguageConfiguration = {
-    wordPattern: /(-?\d*\.\d\w*)|([^\[\{\]\}\:\"\,\s]+)/g,
+  wordPattern: /(-?\d*\.\d\w*)|([^\[\{\]\}\:\"\,\s]+)/g,
 
-    comments: {
-        lineComment: '//',
-        blockComment: ['/*', '*/']
-    },
+  comments: {
+    lineComment: '//',
+    blockComment: ['/*', '*/'],
+  },
 
-    brackets: [
-        ['{', '}'],
-        ['[', ']']
-    ],
+  brackets: [
+    ['{', '}'],
+    ['[', ']'],
+  ],
 
-    autoClosingPairs: [
-        { open: '{', close: '}', notIn: ['string'] },
-        { open: '[', close: ']', notIn: ['string'] },
-        { open: '"', close: '"', notIn: ['string'] }
-    ]
+  autoClosingPairs: [
+    { open: '{', close: '}', notIn: ['string'] },
+    { open: '[', close: ']', notIn: ['string'] },
+    { open: '"', close: '"', notIn: ['string'] },
+  ],
 };
-
