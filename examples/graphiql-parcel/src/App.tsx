c53aa0b8834f467ad71fb70890d23d5bcba6e7f9
daemon1024 <barun1024@gmail.com>
2020-04-22 23:21:43 +0530

    feat : add graphiql parcel-bundler example

---
+++ examples/graphiql-parcel/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import GraphiQL from 'graphiql';
+
+const App = () => (
+  <GraphiQL
+    fetcher={async graphQLParams => {
+      const data = await fetch(
+        'https://swapi-graphql.netlify.app/.netlify/functions/index',
+        {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(graphQLParams),
+          credentials: 'same-origin',
+        },
+      );
+      return data.json().catch(() => data.text());
+    }}
+  />
+);
+
+export default App;
