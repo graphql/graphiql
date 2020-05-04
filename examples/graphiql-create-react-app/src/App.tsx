4ea10adee7b883c001e4cc5eaa31af4bbf65acd0
Barun Acharya <47106543+daemon1024@users.noreply.github.com>
2020-05-04 23:30:20 +0530

    docs: Add graphiql create-react-app example. (#1510)

---
+++ examples/graphiql-create-react-app/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import GraphiQL from 'graphiql';
+import 'graphiql/graphiql.min.css';
+
+const App = () => (
+  <GraphiQL
+    fetcher={async graphQLParams => {
+      const data = await fetch(
+        'https://swapi-graphql.netlify.app/.netlify/functions/index',
+        {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(graphQLParams),
+          credentials: 'same-origin',
+        },
+      );
+      return data.json().catch(() => data.text());
+    }}
+  />
+);
+
+export default App;
