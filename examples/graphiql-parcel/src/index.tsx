fe4b811876838cabdf545a6034ad12bc33e044b2
Harshith Pabbati <pabbatiharshith@gmail.com>
2020-05-05 01:35:38 +0530

    feat: Add Parcel Example for GraphiQL (#1511)

---
+++ examples/graphiql-parcel/src/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GraphiQL from 'graphiql';
+
+const App = () => (
+  <GraphiQL
+    style={{ height: '100vh' }}
+    fetcher={async graphQLParams => {
+      const data = await fetch(
+        'https://swapi-graphql.netlify.app/.netlify/functions/index',
+        {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(graphQLParams),
+          credentials: 'same-origin',
+        },
+      );
+      return data.json().catch(() => data.text());
+    }}
+  />
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// Hot Module Replacement
+if (module.hot) {
+  module.hot.accept();
+}
