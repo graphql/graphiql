a62a4ecb56fd1eccfa43b3a7f88aaabe46d043fb
Benjie Gillam <benjie@jemjie.com>
2020-06-25 20:35:02 +0100

    feat(merge): make fragment merging type-aware (#1542)

--- packages/graphiql/src/utility/mergeAst.ts
+++ packages/graphiql/src/utility/mergeAst.ts
@@ -1,5 +1,5 @@
 /**
- *  Copyright (c) 2019 GraphQL Contributors.
+ *  Copyright (c) 2020 GraphQL Contributors.
  *
  *  This source code is licensed under the MIT license found in the
  *  LICENSE file in the root directory of this source tree.
@@ -7,33 +7,124 @@
 
 import {
   DocumentNode,
+  FieldNode,
   FragmentDefinitionNode,
-  visit,
+  GraphQLOutputType,
+  GraphQLSchema,
   SelectionNode,
+  TypeInfo,
+  getNamedType,
+  visit,
+  visitWithTypeInfo,
+  ASTKindToNode,
+  Visitor,
+  ASTNode,
 } from 'graphql';
+import Maybe from 'graphql/tsutils/Maybe';
 
-export function uniqueBy(
+export function uniqueBy<T>(
   array: readonly SelectionNode[],
-  iteratee: (item: any) => any,
+  iteratee: (item: FieldNode) => T,
 ) {
-  const FilteredMap = new Map();
-  const result = [];
+  const FilteredMap = new Map<T, FieldNode>();
+  const result: SelectionNode[] = [];
   for (const item of array) {
-    const uniqeValue = iteratee(item);
-    if (!FilteredMap.has(uniqeValue)) {
-      FilteredMap.set(uniqeValue, true);
+    if (item.kind === 'Field') {
+      const uniqueValue = iteratee(item);
+      const existing = FilteredMap.get(uniqueValue);
+      if (item.directives && item.directives.length) {
+        // Cannot inline fields with directives (yet)
+        const itemClone = { ...item };
+        result.push(itemClone);
+      } else if (existing && existing.selectionSet && item.selectionSet) {
+        // Merge the selection sets
+        existing.selectionSet.selections = [
+          ...existing.selectionSet.selections,
+          ...item.selectionSet.selections,
+        ];
+      } else if (!existing) {
+        const itemClone = { ...item };
+        FilteredMap.set(uniqueValue, itemClone);
+        result.push(itemClone);
+      }
+    } else {
       result.push(item);
     }
   }
   return result;
 }
 
+export function inlineRelevantFragmentSpreads(
+  fragmentDefinitions: {
+    [key: string]: FragmentDefinitionNode | undefined;
+  },
+  selections: readonly SelectionNode[],
+  selectionSetType?: Maybe<GraphQLOutputType>,
+): readonly SelectionNode[] {
+  const selectionSetTypeName = selectionSetType
+    ? getNamedType(selectionSetType).name
+    : null;
+  const outputSelections = [];
+  const seenSpreads = [];
+  for (let selection of selections) {
+    if (selection.kind === 'FragmentSpread') {
+      const fragmentName = selection.name.value;
+      if (!selection.directives || selection.directives.length === 0) {
+        if (seenSpreads.indexOf(fragmentName) >= 0) {
+          /* It's a duplicate - skip it! */
+          continue;
+        } else {
+          seenSpreads.push(fragmentName);
+        }
+      }
+      const fragmentDefinition = fragmentDefinitions[selection.name.value];
+      if (fragmentDefinition) {
+        const { typeCondition, directives, selectionSet } = fragmentDefinition;
+        selection = {
+          kind: 'InlineFragment',
+          typeCondition,
+          directives,
+          selectionSet,
+        };
+      }
+    }
+    if (
+      selection.kind === 'InlineFragment' &&
+      // Cannot inline if there are directives
+      (!selection.directives || selection.directives?.length === 0)
+    ) {
+      const fragmentTypeName = selection.typeCondition
+        ? selection.typeCondition.name.value
+        : null;
+      if (!fragmentTypeName || fragmentTypeName === selectionSetTypeName) {
+        outputSelections.push(
+          ...inlineRelevantFragmentSpreads(
+            fragmentDefinitions,
+            selection.selectionSet.selections,
+            selectionSetType,
+          ),
+        );
+        continue;
+      }
+    }
+    outputSelections.push(selection);
+  }
+  return outputSelections;
+}
+
 /**
- * Given a document AST, inline all named fragment definitions
+ * Given a document AST, inline all named fragment definitions.
  */
-export default function mergeAST(documentAST: DocumentNode): DocumentNode {
+export default function mergeAST(
+  documentAST: DocumentNode,
+  schema?: GraphQLSchema,
+): DocumentNode {
+  // If we're given the schema, we can simplify even further by resolving object
+  // types vs unions/interfaces
+  const typeInfo = schema ? new TypeInfo(schema) : null;
+
   const fragmentDefinitions: {
-    [key: string]: FragmentDefinitionNode;
+    [key: string]: FragmentDefinitionNode | undefined;
   } = Object.create(null);
 
   for (const definition of documentAST.definitions) {
@@ -42,24 +133,33 @@ export default function mergeAST(documentAST: DocumentNode): DocumentNode {
     }
   }
 
-  return visit(documentAST, {
-    FragmentSpread(node) {
-      return {
-        ...fragmentDefinitions[node.name.value],
-        kind: 'InlineFragment',
-      };
-    },
+  const visitors: Visitor<ASTKindToNode, ASTNode> = {
     SelectionSet(node) {
+      const selectionSetType = typeInfo ? typeInfo.getParentType() : null;
+      let { selections } = node;
+
+      selections = inlineRelevantFragmentSpreads(
+        fragmentDefinitions,
+        selections,
+        selectionSetType,
+      );
+
+      selections = uniqueBy(selections, selection =>
+        selection.alias ? selection.alias.value : selection.name.value,
+      );
+
       return {
         ...node,
-        selections: uniqueBy(
-          node.selections,
-          selection => selection.name.value,
-        ),
+        selections,
       };
     },
     FragmentDefinition() {
       return null;
     },
-  });
+  };
+
+  return visit(
+    documentAST,
+    typeInfo ? visitWithTypeInfo(typeInfo, visitors) : visitors,
+  );
 }
