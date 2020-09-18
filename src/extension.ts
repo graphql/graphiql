7865e481739c698722e96d6c759f10a17314c772
Arnaud Rocher <cailloumajor@users.noreply.github.com>
2020-09-18 17:45:00 +0200

    improvement: associate with graphqls file extension (#211)

--- src/extension.ts
+++ src/extension.ts
@@ -73,7 +73,7 @@ export async function activate(context: ExtensionContext) {
     ],
     synchronize: {
       fileEvents: workspace.createFileSystemWatcher(
-        "**/*.{graphql,gql,js,jsx,ts,tsx}",
+        "**/*.{graphql,graphqls,gql,js,jsx,ts,tsx}",
       ),
     },
     outputChannel: outputChannel,
