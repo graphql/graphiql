e76e7831d62aba24a755e488492a2710f92b4cbd
Anoop M D <anoop.md1421@gmail.com>
2020-10-02 23:51:21 +0530

    fix: Fixed build issues

--- examples/monaco-graphql-webpack/babel.config.js
+++ examples/monaco-graphql-webpack/babel.config.js
@@ -1,14 +1,1 @@
-module.exports = {
-  sourceMaps: true,
-  presets: [
-    [
-      require.resolve('@babel/preset-env'),
-      {
-        targets: { browsers: ['last 2 chrome versions'] },
-        bugfixes: true,
-      },
-    ],
-    [require.resolve('@babel/preset-typescript'), {}],
-  ],
-  plugins: [require.resolve('@babel/plugin-proposal-class-properties')],
-};
+module.exports = require('../../babel.config');
