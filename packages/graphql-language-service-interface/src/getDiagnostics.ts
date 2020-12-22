01d810e032655daaaef2e9004a978fc77189d649
Matt Krick <matt.krick@gmail.com>
2020-12-22 12:41:41 -0800

    remove assert

--- packages/graphql-language-service-interface/src/getDiagnostics.ts
+++ packages/graphql-language-service-interface/src/getDiagnostics.ts
@@ -52,7 +52,7 @@ export const DIAGNOSTIC_SEVERITY = {
 const invariant = (
   condition: any,
   message: string,
-): asserts condition => {
+) => {
   if (!condition) {
     throw new Error(message);
   }
