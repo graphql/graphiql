249b28219b08410dbaeeb1f69b0cbc6ecec4677b
Rikki <rikki.schulte@gmail.com>
2021-01-08 13:14:43 -0500

    feat: export more components and utilities from GraphiQL

--- packages/graphiql/src/index.ts
+++ packages/graphiql/src/index.ts
@@ -4,7 +4,38 @@
  *  This source code is licensed under the MIT license found in the
  *  LICENSE file in the root directory of this source tree.
  */
-import { GraphiQL } from './components/GraphiQL';
 
-export { GraphiQL };
+/**
+ * Components
+ */
+import { GraphiQL } from './components/GraphiQL';
 export default GraphiQL;
+
+export * from './components/GraphiQL';
+
+export { QueryEditor } from './components/QueryEditor';
+export { VariableEditor } from './components/VariableEditor';
+
+// DocExplorer
+export { DocExplorer } from './components/DocExplorer';
+export { default as TypeDoc } from './components/DocExplorer/TypeDoc';
+export { default as SchemaDoc } from './components/DocExplorer/SchemaDoc';
+export { default as FieldDoc } from './components/DocExplorer/FieldDoc';
+export { default as Argument } from './components/DocExplorer/Argument';
+export { default as TypeLink } from './components/DocExplorer/TypeLink';
+export { default as SearchResults } from './components/DocExplorer/SearchResults';
+
+// Toolbar
+export { ToolbarMenu } from './components/ToolbarMenu';
+export { ToolbarButton } from './components/ToolbarButton';
+export { ToolbarGroup } from './components/ToolbarGroup';
+export { ToolbarSelect } from './components/ToolbarSelect';
+
+/**
+ * Utilities
+ */
+export { fillLeafs } from './utility/fillLeafs';
+export { default as mergeAst } from './utility/mergeAst';
+export { getQueryFacts } from './utility/getQueryFacts';
+export { default as getSelectedOperationName } from './utility/getSelectedOperationName';
+export { default as onHasCompletion } from './utility/onHasCompletion';
