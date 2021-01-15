f9036e74e8d93b5a6c47060abb2c748963a9cf83
Rikki <rikki.schulte@gmail.com>
2021-01-14 22:58:00 -0500

    improvement: export more types and modules

--- packages/graphiql/cypress/support/index.js
+++ packages/graphiql/cypress/support/index.js
@@ -17,7 +17,7 @@
 // import '@cypress/code-coverage/support';
 
 // Import commands.js using ES2015 syntax:
-import './commands';
+// import './commands';
 
 // Alternatively you can use CommonJS syntax:
-// require('./commands')
+require('./commands');
--- packages/graphiql/src/index.ts
+++ packages/graphiql/src/index.ts
@@ -4,7 +4,50 @@
  *  This source code is licensed under the MIT license found in the
  *  LICENSE file in the root directory of this source tree.
  */
-import { GraphiQL } from './components/GraphiQL';
 
+/**
+ * GraphiQL
+ */
+
+import { GraphiQL } from './components/GraphiQL';
 export { GraphiQL };
 export default GraphiQL;
+
+/**
+ * Definitions
+ */
+export type {
+  Fetcher,
+  FetcherOpts,
+  FetcherParams,
+  FetcherResult,
+  FetcherReturnType,
+  GraphiQLProps,
+  SyncFetcherResult,
+} from './components/GraphiQL';
+
+/**
+ * Components
+ */
+
+export { QueryEditor } from './components/QueryEditor';
+export { VariableEditor } from './components/VariableEditor';
+export { DocExplorer } from './components/DocExplorer';
+
+/**
+ * Toolbar
+ */
+export { ToolbarMenu } from './components/ToolbarMenu';
+export { ToolbarButton } from './components/ToolbarButton';
+export { ToolbarGroup } from './components/ToolbarGroup';
+export { ToolbarSelect } from './components/ToolbarSelect';
+
+/**
+ * Utilities
+ */
+export { fillLeafs } from './utility/fillLeafs';
+export { default as mergeAst } from './utility/mergeAst';
+export { getQueryFacts } from './utility/getQueryFacts';
+export { default as getSelectedOperationName } from './utility/getSelectedOperationName';
+export { default as onHasCompletion } from './utility/onHasCompletion';
+export { Storage } from './utility/StorageAPI';
