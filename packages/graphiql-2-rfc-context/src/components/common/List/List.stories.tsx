10b29f8e9d22568f76249664e59c35f22bfa0627
Rikki <rikki.schulte@gmail.com>
2021-01-15 00:28:40 -0500

    chore: update copyright for 2021

--- packages/graphiql-2-rfc-context/src/components/common/List/List.stories.tsx
+++ packages/graphiql-2-rfc-context/src/components/common/List/List.stories.tsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 
 /**
- *  Copyright (c) 2020 GraphQL Contributors.
+ *  Copyright (c) 2021 GraphQL Contributors.
  *
  *  This source code is licensed under the MIT license found in the
  *  LICENSE file in the root directory of this source tree.
