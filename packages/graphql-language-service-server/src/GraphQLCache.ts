8ac05f8b141b6f5cb4449c62ad67a34115490ac8
Rikki Schulte <rikki.schulte@gmail.com>
2021-01-15 00:57:26 -0500

    chore: update copyright for 2021 (#1766)
    2020 -> 2021
    add `LICENSE` file to `graphql-language-service` as well

--- packages/graphql-language-service-server/src/GraphQLCache.ts
+++ packages/graphql-language-service-server/src/GraphQLCache.ts
@@ -1,5 +1,5 @@
 /**
- *  Copyright (c) 2020 GraphQL Contributors
+ *  Copyright (c) 2021 GraphQL Contributors
  *  All rights reserved.
  *
  *  This source code is licensed under the license found in the
