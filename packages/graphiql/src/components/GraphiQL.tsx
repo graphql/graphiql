cb4debd96d60101e62d1267b37dc1c938ab58433
Chirag Kasat <chiragkasat0@gmail.com>
2021-02-03 04:12:26 +0530

    fix: run onToggleDocs when setting docExplorerOpen to false (#1768)

--- packages/graphiql/src/components/GraphiQL.tsx
+++ packages/graphiql/src/components/GraphiQL.tsx
@@ -1484,6 +1484,13 @@ export class GraphiQL extends React.Component<GraphiQLProps, GraphiQLState> {
       const docsSize = app.clientWidth - cursorPos;
 
       if (docsSize < 100) {
+        if (typeof this.props.onToggleDocs === 'function') {
+          this.props.onToggleDocs(!this.state.docExplorerOpen);
+        }
+        this._storage.set(
+          'docExplorerOpen',
+          JSON.stringify(this.state.docExplorerOpen),
+        );
         this.setState({ docExplorerOpen: false });
       } else {
         this.setState({
