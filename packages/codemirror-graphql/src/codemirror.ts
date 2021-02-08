3d09b4a91c98342467b5aebbfd6e10cd1a2feae5
Samuel <samuelimolo4real@gmail.com>
2021-02-08 19:20:56 +0000

    Updated codemirror typing

--- packages/codemirror-graphql/src/codemirror.ts
+++ packages/codemirror-graphql/src/codemirror.ts
@@ -1,15 +1,16 @@
-import CodeMirror, { ShowHintOptions } from 'codemirror';
-import { IHint, IHints } from './hint';
+import CodeMirror from 'codemirror';
 
 declare module 'codemirror' {
   var Init: any;
 
   interface Editor {
     doc: CodeMirror.Doc;
-    getHelper(pos: { line; ch }, type: string): any;
-    getHelpers(pos: { line; ch }, type: string): any[];
+    getHelper(pos: { line: number; ch: number }, type: string): any;
+    getHelpers(pos: { line: number; ch: number }, type: string): any[];
   }
+
   interface ShowHintOptions {
+    // @ts-ignore
     hint?: ShowHintOptions['hint'];
   }
 
@@ -17,3 +18,5 @@ declare module 'codemirror' {
 
   const hint: CodeMirrorHintMap;
 }
+
+export default CodeMirror;
