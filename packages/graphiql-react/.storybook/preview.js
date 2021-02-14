58f3fcda08ae16f5c86cb61ed68631e08d47632e
Ryan Walker <ryan.matthew.walker@gmail.com>
2021-02-13 21:18:15 -0500

    fixed git history

--- packages/graphiql-react/.storybook/preview.js
+++ packages/graphiql-react/.storybook/preview.js
@@ -11,6 +11,4 @@ import { addDecorator } from '@storybook/react';
 
 import ThemeProvider from '../src/themes/provider';
 
-addDecorator(storyFn => {
-  return <ThemeProvider>{storyFn()}</ThemeProvider>;
-});
+addDecorator(story => <ThemeProvider>{story()}</ThemeProvider>);
--- packages/graphiql-react/src/themes/provider.tsx
+++ packages/graphiql-react/src/themes/provider.tsx
@@ -13,34 +13,32 @@ import { Global } from '@emotion/core';
 
 const Reset = () => (
   <Global
-    styles={themeStyles => {
-      return {
-        '*': {
-          margin: 0,
-          padding: 0,
-          boxSizing: 'border-box',
-          listStyle: 'none',
-        },
-        body: {
-          fontFamily: themeStyles.fonts.body,
-          fontSize: themeStyles.fontSizes[1],
-          color: themeStyles.colors.text,
-          backgroundColor: themeStyles.colors.background,
-        },
-        small: {
-          fontSize: '100%',
-        },
-        a: {
-          textDecoration: 'none',
-        },
-        button: {
-          border: 0,
-          padding: 0,
-          fontSize: '100%',
-          backgroundColor: 'transparent',
-        },
-      };
-    }}
+    styles={themeStyles => ({
+      '*': {
+        margin: 0,
+        padding: 0,
+        boxSizing: 'border-box',
+        listStyle: 'none',
+      },
+      body: {
+        fontFamily: themeStyles.fonts.body,
+        fontSize: themeStyles.fontSizes[1],
+        color: themeStyles.colors.text,
+        backgroundColor: themeStyles.colors.background,
+      },
+      small: {
+        fontSize: '100%',
+      },
+      a: {
+        textDecoration: 'none',
+      },
+      button: {
+        border: 0,
+        padding: 0,
+        fontSize: '100%',
+        backgroundColor: 'transparent',
+      },
+    })}
   />
 );
 
