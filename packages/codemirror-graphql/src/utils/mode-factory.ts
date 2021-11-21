2eb774f14d06cb3ef68825f65de82f801dcea6ce
Samuel Imolorhe <samuelimolo4real@gmail.com>
2021-11-21 17:00:07 +0100

    Added Codemirror 6 legacy mode support
    Implemented the codemirror.next legacy mode support for graphql mode.
    This only covers syntax highlighting. Autocomplete, jump and info extensions
    still need to be implemented to have feature parity with Codemirror v5.
    
    From #2022

---
+++ packages/codemirror-graphql/src/utils/mode-factory.ts
@@ -0,0 +1,64 @@
+/**
+ *  Copyright (c) 2021 GraphQL Contributors
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+import CodeMirror from 'codemirror';
+import {
+  LexRules,
+  ParseRules,
+  isIgnored,
+  onlineParser,
+} from 'graphql-language-service-parser';
+import indent from './mode-indent';
+
+/**
+ * The GraphQL mode is defined as a tokenizer along with a list of rules, each
+ * of which is either a function or an array.
+ *
+ *   * Function: Provided a token and the stream, returns an expected next step.
+ *   * Array: A list of steps to take in order.
+ *
+ * A step is either another rule, or a terminal description of a token. If it
+ * is a rule, that rule is pushed onto the stack and the parsing continues from
+ * that point.
+ *
+ * If it is a terminal description, the token is checked against it using a
+ * `match` function. If the match is successful, the token is colored and the
+ * rule is stepped forward. If the match is unsuccessful, the remainder of the
+ * rule is skipped and the previous rule is advanced.
+ *
+ * This parsing algorithm allows for incremental online parsing within various
+ * levels of the syntax tree and results in a structured `state` linked-list
+ * which contains the relevant information to produce valuable typeaheads.
+ */
+const graphqlModeFactory: CodeMirror.ModeFactory<any> = config => {
+  const parser = onlineParser({
+    eatWhitespace: stream => stream.eatWhile(isIgnored),
+    lexRules: LexRules,
+    parseRules: ParseRules,
+    editorConfig: { tabSize: config.tabSize },
+  });
+
+  return {
+    config,
+    startState: parser.startState,
+    token: (parser.token as unknown) as NonNullable<
+      CodeMirror.Mode<any>['token']
+    >, // TODO: Check if the types are indeed compatible
+    indent,
+    electricInput: /^\s*[})\]]/,
+    fold: 'brace',
+    lineComment: '#',
+    closeBrackets: {
+      pairs: '()[]{}""',
+      explode: '()[]{}',
+    },
+  };
+};
+
+export default graphqlModeFactory;
---
+++ packages/codemirror-graphql/src/utils/mode-indent.ts
@@ -0,0 +1,31 @@
+/**
+ *  Copyright (c) 2021 GraphQL Contributors
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+import CodeMirror from 'codemirror';
+import { State } from 'graphql-language-service-parser';
+
+// Seems the electricInput type in @types/codemirror is wrong (i.e it is written as electricinput instead of electricInput)
+export default function indent(
+  this: CodeMirror.Mode<any> & {
+    electricInput?: RegExp;
+    config?: CodeMirror.EditorConfiguration;
+  },
+  state: State,
+  textAfter: string,
+) {
+  const levels = state.levels;
+  // If there is no stack of levels, use the current level.
+  // Otherwise, use the top level, pre-emptively dedenting for close braces.
+  const level =
+    !levels || levels.length === 0
+      ? state.indentLevel
+      : levels[levels.length - 1] -
+        (this.electricInput?.test(textAfter) ? 1 : 0);
+  return (level || 0) * (this.config?.indentUnit || 0);
+}
