ad2f6fb5d3fd30021adb500a7cbcef596fe5b35f
Rikki Schulte <rikki.schulte@gmail.com>
2021-12-10 18:58:39 +0100

    more

--- packages/graphiql-react/src/example/App.tsx
+++ packages/graphiql-react/src/example/App.tsx
@@ -1,16 +1,83 @@
 import React, { useEffect, useLayoutEffect, useRef } from 'react';
-import * as monaco from 'monaco-editor'
-import "monaco-graphql"
+import { getIntrospectionQuery, IntrospectionQuery } from 'graphql';
+import * as monaco from 'monaco-editor';
+import { initializeMode } from 'monaco-graphql/esm/initializeMode';
 
+import { createGraphiQLFetcher } from '@graphiql/toolkit';
 
+const fetcher = createGraphiQLFetcher({
+  url: 'https://api.spacex.land/graphql/',
+});
+
+const getSchema = async () =>
+  fetcher({
+    query: getIntrospectionQuery(),
+    operationName: 'IntrospectionQuery',
+  });
+
+const getOrCreateModel = (uri: string, value: string) => {
+  return (
+    monaco.editor.getModel(monaco.Uri.file(uri)) ??
+    monaco.editor.createModel(value, uri.split('.').pop(), monaco.Uri.file(uri))
+  );
+};
 export default function App() {
-    const editorRef = useRef()
-    useLayoutEffect(() => {
-        const model = monaco.editor.createModel('query {}', 'graphql', monaco.Uri.file('operation.graphql'))
-        monaco.editor.create(document.getElementById('root')!, {
-            model
-        })
+  const [
+    queryEditor,
+    setQueryEditor,
+  ] = React.useState<monaco.editor.IStandaloneCodeEditor | null>(null);
+  const [
+    variablesEditor,
+    setVariablesEditor,
+  ] = React.useState<monaco.editor.IStandaloneCodeEditor | null>(null);
+  const [schema, setSchema] = React.useState<unknown | null>(null);
+  const [loading, setLoading] = React.useState(false);
 
-    }, [])
-  return <div ref={editorRef.current} />;
+  useEffect(() => {
+    const queryModel = getOrCreateModel(
+      'operation.graphql',
+      'query { payloads }',
+    );
+    const variablesModel = getOrCreateModel('variables.json', '{}');
+    queryEditor ??
+      setQueryEditor(
+        monaco.editor.create(document.getElementById('operations')!, {
+          model: queryModel,
+          theme: 'vs-dark',
+        }),
+      );
+    variablesEditor ??
+      setVariablesEditor(
+        monaco.editor.create(document.getElementById('variables')!, {
+          model: variablesModel,
+          theme: 'vs-dark',
+        }),
+      );
+
+    if (!schema && !loading) {
+      setLoading(true);
+      getSchema()
+        .then(data => {
+            const api = initializeMode();
+          setSchema(data.data);
+          api.setSchemaConfig([
+            {
+              introspectionJSONString: JSON.stringify(data.data),
+              fileMatch: [queryEditor?.getModel().uri],
+              uri: 'myschema.graphql',
+            },
+          ])
+          return
+        })
+        .then(() => setLoading(false));
+    }
+  }, [queryEditor, variablesEditor, schema, loading]);
+  return (
+    <div id="wrapper">
+      <div id="left-pane" className="pane">
+        <div id="operations" className="editor" />
+        <div id="variables" className="editor" />
+      </div>
+    </div>
+  );
 }
