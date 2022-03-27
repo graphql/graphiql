acded62d659492b95e980db3610dcd4e35f98424
timsuchanek <tim.suchanek@gmail.com>
2022-03-27 11:06:50 +0200

    fix: try operationName, then fuzzy approach

--- packages/graphiql/src/utility/fuzzyExtractOperationTitle.ts
+++ packages/graphiql/src/utility/fuzzyExtractOperationTitle.ts
@@ -1,7 +1,6 @@
 /**
  * Very simple and quick way of extracting the operation title from a document string
  * (compared to parsing and traversing the whole AST).
- * We also use this instead of graphql parse in order to support incomplete documents
  */
 export function fuzzyExtractOperationTitle(str: string): string {
   const regex = /^(?!.*#).*(query|subscription|mutation)\s+([a-zA-Z0-9_]+)/;
