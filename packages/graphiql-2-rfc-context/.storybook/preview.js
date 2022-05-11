a63ff958838cf4fcf31f7eaa3e3b022d02838f65
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2022-05-11 22:48:47 +0200

    chore(deps): use React v17 consistently (#2397)
    * update example/graphiql-create-react-app
    
    * update examples/graphiql-parcel
    
    * fix codemirror imports for parcel example
    
    * update graphiql
    
    * update graphiql-cdn
    
    * upgrade storybook to avoid react 16 as dependency
    
    * update graphiql-webpack
    
    * update examples/monaco-graphql-webpack
    
    * add changeset

--- packages/graphiql-2-rfc-context/.storybook/preview.js
+++ packages/graphiql-2-rfc-context/.storybook/preview.js
@@ -6,14 +6,8 @@
  *
  */
 
-import { configure, addDecorator } from '@storybook/react';
+import { addDecorator } from '@storybook/react';
 import React from 'react';
-import requireContext from 'require-context.macro';
 import ThemeProvider from '../src/components/common/themes/provider';
 
 addDecorator(story => <ThemeProvider>{story()}</ThemeProvider>);
-
-configure(
-  requireContext('../src/components/common', true, /\.stories\.tsx$/),
-  module,
-);
