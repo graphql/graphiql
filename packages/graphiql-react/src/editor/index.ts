dc666ac2d3e45d331614c86587ed68af34275392
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2022-05-16 10:46:08 +0200

    fix docs build

--- packages/graphiql-react/src/editor/index.ts
+++ packages/graphiql-react/src/editor/index.ts
@@ -1,2 +1,9 @@
-export * from './context';
-export * from './header-editor';
+import { EditorContext, EditorContextProvider } from './context';
+import { useHeaderEditor } from './header-editor';
+
+import type { EditorContextType } from './context';
+import type { UseHeaderEditorArgs } from './header-editor';
+
+export { EditorContext, EditorContextProvider, useHeaderEditor };
+
+export type { EditorContextType, UseHeaderEditorArgs };
