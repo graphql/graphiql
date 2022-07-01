a2685313a50f1346c220fe1b088488b724905c5d
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2022-07-01 12:06:34 +0200

    move editor styles to `@graphiql/react` (#2532)

--- packages/graphiql-react/src/editor/components/response-editor.tsx
+++ packages/graphiql-react/src/editor/components/response-editor.tsx
@@ -3,6 +3,7 @@ import { useResponseEditor, UseResponseEditorArgs } from '../response-editor';
 import '../style/codemirror.css';
 import '../style/fold.css';
 import '../style/info.css';
+import '../style/editor.css';
 
 export function ResponseEditor(props: UseResponseEditorArgs) {
   const ref = useResponseEditor(props);
