6beb9dda231a97f7fc2b8e77d96fae9153cd49a4
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2022-07-04 17:27:52 +0200

    add caller argument to editor hooks (#2538)

--- packages/graphiql-react/src/editor/query-editor.tsx
+++ packages/graphiql-react/src/editor/query-editor.tsx
@@ -52,20 +52,23 @@ export type UseQueryEditorArgs = {
   keyMap?: KeyMap;
 };
 
-export function useQueryEditor({
-  editorTheme = DEFAULT_EDITOR_THEME,
-  keyMap = DEFAULT_KEY_MAP,
-  externalFragments,
-  onClickReference,
-  onCopyQuery,
-  onEdit,
-  onEditOperationName,
-  readOnly = false,
-  validationRules,
-}: UseQueryEditorArgs = {}) {
+export function useQueryEditor(
+  {
+    editorTheme = DEFAULT_EDITOR_THEME,
+    keyMap = DEFAULT_KEY_MAP,
+    externalFragments,
+    onClickReference,
+    onCopyQuery,
+    onEdit,
+    onEditOperationName,
+    readOnly = false,
+    validationRules,
+  }: UseQueryEditorArgs = {},
+  caller?: Function,
+) {
   const { schema } = useSchemaContext({
     nonNull: true,
-    caller: useQueryEditor,
+    caller: caller || useQueryEditor,
   });
   const {
     initialQuery,
@@ -75,14 +78,14 @@ export function useQueryEditor({
     updateActiveTabValues,
   } = useEditorContext({
     nonNull: true,
-    caller: useQueryEditor,
+    caller: caller || useQueryEditor,
   });
   const executionContext = useExecutionContext();
   const storage = useStorageContext();
   const explorer = useExplorerContext();
-  const copy = useCopyQuery({ caller: useQueryEditor, onCopyQuery });
-  const merge = useMergeQuery({ caller: useQueryEditor });
-  const prettify = usePrettifyEditors({ caller: useQueryEditor });
+  const copy = useCopyQuery({ caller: caller || useQueryEditor, onCopyQuery });
+  const merge = useMergeQuery({ caller: caller || useQueryEditor });
+  const prettify = usePrettifyEditors({ caller: caller || useQueryEditor });
   const ref = useRef<HTMLDivElement>(null);
   const codeMirrorRef = useRef<CodeMirrorType>();
 
