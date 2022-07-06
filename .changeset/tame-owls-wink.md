8d97fb8e781c05552e5aba1d2de7fd9ed3e477ac
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2022-07-06 14:32:35 +0200

    avoid top-level codemirror imports

---
+++ .changeset/tame-owls-wink.md
@@ -0,0 +1,5 @@
+---
+'@graphiql/react': patch
+---
+
+Avoid top-level imports from `codemirror` that break importing the package in non-browser environments
--- packages/graphiql-react/src/editor/common.ts
+++ packages/graphiql-react/src/editor/common.ts
@@ -17,20 +17,6 @@ export const commonKeys = {
   'Alt-Right': 'goGroupRight',
 };
 
-export const commonCodeMirrorAddons = [
-  import('codemirror/addon/hint/show-hint'),
-  import('codemirror/addon/edit/matchbrackets'),
-  import('codemirror/addon/edit/closebrackets'),
-  import('codemirror/addon/fold/brace-fold'),
-  import('codemirror/addon/fold/foldgutter'),
-  import('codemirror/addon/lint/lint'),
-  import('codemirror/addon/search/searchcursor'),
-  import('codemirror/addon/search/jump-to-line'),
-  import('codemirror/addon/dialog/dialog'),
-  // @ts-expect-error
-  import('codemirror/keymap/sublime'),
-];
-
 /**
  * Dynamically import codemirror and dependencies
  * This works for codemirror 5, not sure if the same imports work for 6
@@ -47,7 +33,20 @@ export async function importCodeMirror(
   const allAddons =
     options?.useCommonAddons === false
       ? addons
-      : commonCodeMirrorAddons.concat(addons);
+      : [
+          import('codemirror/addon/hint/show-hint'),
+          import('codemirror/addon/edit/matchbrackets'),
+          import('codemirror/addon/edit/closebrackets'),
+          import('codemirror/addon/fold/brace-fold'),
+          import('codemirror/addon/fold/foldgutter'),
+          import('codemirror/addon/lint/lint'),
+          import('codemirror/addon/search/searchcursor'),
+          import('codemirror/addon/search/jump-to-line'),
+          import('codemirror/addon/dialog/dialog'),
+          // @ts-expect-error
+          import('codemirror/keymap/sublime'),
+          ...addons,
+        ];
   await Promise.all(allAddons.map(addon => addon));
   return CodeMirror;
 }
