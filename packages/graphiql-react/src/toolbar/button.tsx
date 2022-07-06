a3cb25cfb2f96349561d75ea4d0d0b1cc5e873db
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2022-07-06 10:22:47 +0200

    [redesign] implement new toolbar design (#2524)
    * implement new toolbar design
    
    * add `caller` argument for better errors

---
+++ packages/graphiql-react/src/toolbar/button.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+import { UnStyledButton } from '../ui';
+
+import './button.css';
+
+export function ToolbarButton(props: JSX.IntrinsicElements['button']) {
+  const [error, setError] = useState<Error | null>(null);
+  return (
+    <UnStyledButton
+      {...props}
+      className={
+        'graphiql-toolbar-button' +
+        (error ? ' error' : '') +
+        (props.className ? ' ' + props.className : '')
+      }
+      onClick={event => {
+        try {
+          props.onClick?.(event);
+          setError(null);
+        } catch (err) {
+          setError(
+            err instanceof Error
+              ? err
+              : new Error(`Toolbar button click failed: ${err}`),
+          );
+        }
+      }}
+      title={error ? error.message : props.title}
+      aria-invalid={error ? 'true' : props['aria-invalid']}
+    />
+  );
+}
