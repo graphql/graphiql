686bc6e01ed19571af9893be3c347de6b1211180
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2022-07-11 09:49:59 +0200

    [redesign] implement sidebar (#2543)
    * implement sidebar in new design
    
    * simplify callback

--- packages/graphiql-react/src/icons/index.tsx
+++ packages/graphiql-react/src/icons/index.tsx
@@ -1,9 +1,14 @@
 import _ChevronDownIcon from './chevron-down.svg';
 import _ChevronUpIcon from './chevron-up.svg';
 import _CopyIcon from './copy.svg';
+import _DocsIcon from './docs.svg';
+import _HistoryIcon from './history.svg';
+import _KeyboardShortcutIcon from './keyboard-shortcut.svg';
 import _MergeIcon from './merge.svg';
 import _PlayIcon from './play.svg';
 import _PrettifyIcon from './prettify.svg';
+import _ReloadIcon from './reload.svg';
+import _SettingsIcon from './settings.svg';
 import _StopIcon from './stop.svg';
 
 export const ChevronDownIcon = generateIcon(
@@ -12,9 +17,17 @@ export const ChevronDownIcon = generateIcon(
 );
 export const ChevronUpIcon = generateIcon(_ChevronUpIcon, 'chevron up icon');
 export const CopyIcon = generateIcon(_CopyIcon, 'copy icon');
+export const DocsIcon = generateIcon(_DocsIcon, 'docs icon');
+export const HistoryIcon = generateIcon(_HistoryIcon, 'history icon');
+export const KeyboardShortcutIcon = generateIcon(
+  _KeyboardShortcutIcon,
+  'keyboard shortcut icon',
+);
 export const MergeIcon = generateIcon(_MergeIcon, 'merge icon');
 export const PlayIcon = generateIcon(_PlayIcon, 'play icon');
 export const PrettifyIcon = generateIcon(_PrettifyIcon, 'prettify icon');
+export const ReloadIcon = generateIcon(_ReloadIcon, 'reload icon');
+export const SettingsIcon = generateIcon(_SettingsIcon, 'settings icon');
 export const StopIcon = generateIcon(_StopIcon, 'stop icon');
 
 function generateIcon(RawComponent: any, title: string) {
