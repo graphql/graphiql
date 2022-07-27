a3b99d5e0bb0616a84f34e68b97efa6f4ab0f819
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2022-07-27 12:56:12 +0200

    move remaining doc explorer component to `@graphiql/react` (#2587)

--- packages/graphiql-react/src/explorer/index.ts
+++ packages/graphiql-react/src/explorer/index.ts
@@ -1,8 +1,12 @@
 export { Argument } from './components/argument';
 export { DefaultValue } from './components/default-value';
 export { Directive } from './components/directive';
+export { DocExplorer } from './components/doc-explorer';
+export { FieldDocumentation } from './components/field-documentation';
 export { FieldLink } from './components/field-link';
+export { SchemaDocumentation } from './components/schema-documentation';
 export { Search } from './components/search';
+export { TypeDocumentation } from './components/type-documentation';
 export { TypeLink } from './components/type-link';
 export {
   ExplorerContext,
