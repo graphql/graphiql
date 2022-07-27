c80d30f337026c142e9563e1c1c677aba01e032e
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2022-07-27 13:42:03 +0200

    [redesign] docs (#2588)
    * add icons
    
    * add `Button` component to `@graphiql/react`
    
    * add explorer section component
    
    * redesign `SchemaDocumentation` component
    
    * redesign `TypeDocumentation` component
    
    * redesign `FieldDocumentation` component
    
    * redesign `DocExplorer` component
    
    * extend changeset message

---
+++ packages/graphiql-react/src/explorer/components/deprecation-reason.tsx
@@ -0,0 +1,16 @@
+import { MarkdownContent } from '../../ui';
+
+import './deprecation-reason.css';
+
+type DeprecationReasonProps = { children?: string | null };
+
+export function DeprecationReason(props: DeprecationReasonProps) {
+  return props.children ? (
+    <div className="graphiql-doc-explorer-deprecation">
+      <div className="graphiql-doc-explorer-deprecation-label">Deprecated</div>
+      <MarkdownContent type="deprecation" onlyShowFirstChild>
+        {props.children}
+      </MarkdownContent>
+    </div>
+  ) : null;
+}
