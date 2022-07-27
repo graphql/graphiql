abbfad9d7d059808cfe4f88ae2bc53738f959c2a
Foo-x <www.sand.x@gmail.com>
2022-07-27 23:08:22 +0900

    feat: support vscode multi-root workspaces

--- packages/vscode-graphql/src/extension.ts
+++ packages/vscode-graphql/src/extension.ts
@@ -4,6 +4,8 @@ import {
   window,
   commands,
   OutputChannel,
+  TextDocument,
+  Uri,
 } from 'vscode';
 
 import {
@@ -17,93 +19,134 @@ import {
 import * as path from 'path';
 import { createStatusBar, initStatusBar } from './apis/statusBar';
 
-let client: LanguageClient;
+const clients: Map<string, LanguageClient> = new Map();
 
 export async function activate(context: ExtensionContext) {
   const outputChannel: OutputChannel = window.createOutputChannel(
     'GraphQL Language Server',
   );
 
-  const config = getConfig();
-  const { debug } = config;
-  if (debug) {
-    console.log('Extension "vscode-graphql" is now active!');
-  }
-
-  const serverPath = path.join('out', 'server', 'index.js');
-  const serverModule = context.asAbsolutePath(serverPath);
-
-  const debugOptions = {
-    execArgv: ['--nolazy', '--inspect=localhost:6009'],
-  };
-
-  const serverOptions: ServerOptions = {
-    run: {
-      module: serverModule,
-      transport: TransportKind.ipc,
-    },
-    debug: {
-      module: serverModule,
-      transport: TransportKind.ipc,
-      options: { ...(debug ? debugOptions : {}) },
-    },
-  };
-
-  const clientOptions: LanguageClientOptions = {
-    documentSelector: [
-      { scheme: 'file', language: 'graphql' },
-      { scheme: 'file', language: 'javascript' },
-      { scheme: 'file', language: 'javascriptreact' },
-      { scheme: 'file', language: 'typescript' },
-      { scheme: 'file', language: 'typescriptreact' },
-    ],
-    synchronize: {
-      // TODO: This should include any referenced graphql files inside the graphql-config
-      fileEvents: [
-        workspace.createFileSystemWatcher(
-          '/{graphql.config.*,.graphqlrc,.graphqlrc.*,package.json}',
-          false,
-          // Ignore change events for graphql config, we only care about create, delete and save events
-          // otherwise, the underlying language service is re-started on every key change.
-          // also, it makes sense that it should only re-load on file save, but we need to document that.
-          // TODO: perhaps we can intercept change events, and remind the user
-          // to save for the changes to take effect
-          true,
-        ),
-        // These ignore node_modules and .git by default
-        workspace.createFileSystemWatcher(
-          '**/{*.graphql,*.graphqls,*.gql,*.js,*.mjs,*.cjs,*.esm,*.es,*.es6,*.jsx,*.ts,*.tsx}',
-        ),
-      ],
-    },
-    outputChannel,
-    outputChannelName: 'GraphQL Language Server',
-    revealOutputChannelOn: RevealOutputChannelOn.Never,
-    initializationFailedHandler: err => {
-      outputChannel.appendLine('Initialization failed');
-      outputChannel.appendLine(err.message);
-      if (err.stack) {
-        outputChannel.appendLine(err.stack);
-      }
-      if (debug) {
-        outputChannel.show();
-      }
-      return false;
-    },
-  };
-
-  client = new LanguageClient(
-    'vscode-graphql',
-    serverOptions,
-    clientOptions,
-    debug,
-  );
-
   const statusBarItem = createStatusBar();
   context.subscriptions.push(statusBarItem);
 
-  await client.start();
-  initStatusBar(statusBarItem, client, window.activeTextEditor);
+  async function onDidOpenTextDocument(document: TextDocument): Promise<void> {
+    const config = getConfig(document.uri);
+    const { debug } = config;
+    if (debug) {
+      console.log('Extension "vscode-graphql" is now active!');
+    }
+
+    const serverPath = path.join('out', 'server', 'index.js');
+    const serverModule = context.asAbsolutePath(serverPath);
+
+    const folder = workspace.getWorkspaceFolder(document.uri);
+    if (folder === undefined || clients.has(folder.uri.toString())) {
+      return;
+    }
+
+    const debugOptions = {
+      execArgv: ['--nolazy', `--inspect=localhost:${6009 + clients.size}`],
+    };
+
+    const serverOptions: ServerOptions = {
+      run: {
+        module: serverModule,
+        transport: TransportKind.ipc,
+      },
+      debug: {
+        module: serverModule,
+        transport: TransportKind.ipc,
+        options: { ...(debug ? debugOptions : {}) },
+      },
+    };
+
+    const clientOptions: LanguageClientOptions = {
+      documentSelector: [
+        {
+          scheme: 'file',
+          language: 'graphql',
+          pattern: `${folder.uri.fsPath}/**/*`,
+        },
+        {
+          scheme: 'file',
+          language: 'javascript',
+          pattern: `${folder.uri.fsPath}/**/*`,
+        },
+        {
+          scheme: 'file',
+          language: 'javascriptreact',
+          pattern: `${folder.uri.fsPath}/**/*`,
+        },
+        {
+          scheme: 'file',
+          language: 'typescript',
+          pattern: `${folder.uri.fsPath}/**/*`,
+        },
+        {
+          scheme: 'file',
+          language: 'typescriptreact',
+          pattern: `${folder.uri.fsPath}/**/*`,
+        },
+      ],
+      synchronize: {
+        // TODO: This should include any referenced graphql files inside the graphql-config
+        fileEvents: [
+          workspace.createFileSystemWatcher(
+            `${folder.uri.fsPath}/{graphql.config.*,.graphqlrc,.graphqlrc.*,package.json}`,
+            false,
+            // Ignore change events for graphql config, we only care about create, delete and save events
+            // otherwise, the underlying language service is re-started on every key change.
+            // also, it makes sense that it should only re-load on file save, but we need to document that.
+            // TODO: perhaps we can intercept change events, and remind the user
+            // to save for the changes to take effect
+            true,
+          ),
+          // These ignore node_modules and .git by default
+          workspace.createFileSystemWatcher(
+            `${folder.uri.fsPath}/**/{*.graphql,*.graphqls,*.gql,*.js,*.mjs,*.cjs,*.esm,*.es,*.es6,*.jsx,*.ts,*.tsx}`,
+          ),
+        ],
+      },
+      outputChannel,
+      outputChannelName: 'GraphQL Language Server',
+      revealOutputChannelOn: RevealOutputChannelOn.Never,
+      initializationFailedHandler: err => {
+        outputChannel.appendLine('Initialization failed');
+        outputChannel.appendLine(err.message);
+        if (err.stack) {
+          outputChannel.appendLine(err.stack);
+        }
+        if (debug) {
+          outputChannel.show();
+        }
+        return false;
+      },
+      workspaceFolder: folder,
+    };
+
+    const client = new LanguageClient(
+      'vscode-graphql',
+      serverOptions,
+      clientOptions,
+      debug,
+    );
+
+    await client.start();
+    clients.set(folder.uri.toString(), client);
+    initStatusBar(statusBarItem, client, window.activeTextEditor);
+  }
+
+  workspace.onDidOpenTextDocument(onDidOpenTextDocument);
+  workspace.textDocuments.forEach(onDidOpenTextDocument);
+  workspace.onDidChangeWorkspaceFolders(event => {
+    for (const folder of event.removed) {
+      const client = clients.get(folder.uri.toString());
+      if (client) {
+        clients.delete(folder.uri.toString());
+        client.stop();
+      }
+    }
+  });
 
   const commandShowOutputChannel = commands.registerCommand(
     'vscode-graphql.showOutputChannel',
@@ -114,25 +157,35 @@ export async function activate(context: ExtensionContext) {
 
   commands.registerCommand('vscode-graphql.restart', async () => {
     outputChannel.appendLine(`Stopping GraphQL LSP`);
-    await client.stop();
+    await stopClients();
 
     outputChannel.appendLine(`Restarting GraphQL LSP`);
-    await client.start();
+    await startClients();
     outputChannel.appendLine(`GraphQL LSP restarted`);
   });
 }
 
 export function deactivate() {
-  if (!client) {
-    return undefined;
-  }
   console.log('Extension "vscode-graphql" will be de-activated!!');
-  return client.stop();
+  return stopClients();
 }
 
-function getConfig() {
-  return workspace.getConfiguration(
-    'vscode-graphql',
-    window.activeTextEditor ? window.activeTextEditor.document.uri : null,
-  );
+function getConfig(resource: Uri) {
+  return workspace.getConfiguration('vscode-graphql', resource);
+}
+
+async function stopClients() {
+  const promises: Promise<void>[] = [];
+  for (const client of clients.values()) {
+    promises.push(client.stop());
+  }
+  return Promise.all(promises).then(() => undefined);
+}
+
+async function startClients() {
+  const promises: Promise<void>[] = [];
+  for (const client of clients.values()) {
+    promises.push(client.start());
+  }
+  return Promise.all(promises).then(() => undefined);
 }
