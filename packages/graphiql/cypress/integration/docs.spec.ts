da6bfb24f87df2eb662ee35647cc967605a88b7c
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2022-07-27 13:42:03 +0200

    [redesign] docs (#2588)
    * add icons
    
    * add `Button` component to `@graphiql/react`
    
    * add explorer section component
    
    * redesign `SchemaDocumentation` component
    
    * redesign `TypeDocumentation` component
    
    * redesign `FieldDocumentation` component
    
    * redesign `DocExplorer` component
    
    * extend changeset message

--- packages/graphiql/cypress/integration/docs.spec.ts
+++ packages/graphiql/cypress/integration/docs.spec.ts
@@ -6,13 +6,12 @@ describe('GraphiQL DocExplorer - button', () => {
   });
   it('Toggles doc pane on', () => {
     cy.get('.graphiql-sidebar button').eq(0).click();
-    cy.get('.doc-explorer').should('be.visible');
+    cy.get('.graphiql-doc-explorer').should('be.visible');
   });
 
   it('Toggles doc pane back off', () => {
-    // there are two components with .docExplorerHide, one in query history
-    cy.get('.graphiql-plugin button.docExplorerHide').click();
-    cy.get('.doc-explorer').should('not.be.visible');
+    cy.get('.graphiql-sidebar button').eq(0).click();
+    cy.get('.graphiql-doc-explorer').should('not.be.visible');
   });
 });
 
@@ -30,7 +29,7 @@ describe('GraphiQL DocExplorer - search', () => {
 
   it('Navigates to a docs entry on selecting a search result', () => {
     cy.get('[data-reach-combobox-option]').eq(4).children().click();
-    cy.get('.doc-explorer-title').should('have.text', 'TestInput');
+    cy.get('.graphiql-doc-explorer-title').should('have.text', 'TestInput');
   });
 
   it('Allows searching fields within a type', () => {
@@ -45,10 +44,14 @@ describe('GraphiQL DocExplorer - search', () => {
     cy.get('[data-reach-combobox-option]').contains('hasArgs');
   });
 
-  it('Navigates back and closes popover', () => {
-    cy.get('.doc-explorer-back').click();
-    cy.get('.doc-explorer-title').should('have.text', 'Docs');
+  it('Closes popover when blurring input', () => {
+    cy.get('[data-reach-combobox-input]').blur();
     cy.get('[data-reach-combobox-popover]').should('have.attr', 'hidden');
+  });
+
+  it('Navigates back', () => {
+    cy.get('.graphiql-doc-explorer-back').click();
+    cy.get('.graphiql-doc-explorer-title').should('have.text', 'Docs');
   });
 
   it('Type fields link to their own docs entry', () => {
@@ -56,7 +59,7 @@ describe('GraphiQL DocExplorer - search', () => {
     cy.wait(250);
     cy.get('[data-reach-combobox-option]').last().click();
 
-    cy.get('.doc-explorer-title').should('have.text', 'isTest');
+    cy.get('.graphiql-doc-explorer-title').should('have.text', 'isTest');
     cy.get('.graphiql-markdown-description').should(
       'have.text',
       'Is this a test schema? Sure it is.\n',
@@ -65,27 +68,28 @@ describe('GraphiQL DocExplorer - search', () => {
 });
 
 describe('GraphQL DocExplorer - deprecated fields', () => {
-  before(() => {
-    cy.visit(`/`);
-    cy.get('.graphiql-sidebar button').eq(0).click();
-  });
-  it('should show deprecated fields category title', () => {
-    cy.get('.doc-category>.doc-category-item').first().find('a').click();
-    cy.wait(300);
-    cy.get('.doc-category>.doc-category-title')
-      .last()
-      .should('have.text', 'deprecated fields');
-  });
   it('should show deprecated fields details when expanding', () => {
-    cy.get('.show-btn').click();
+    cy.visit(`/`);
+    // Open doc explorer
+    cy.get('.graphiql-sidebar button').eq(0).click();
 
-    const deprecated = cy.get('.doc-category').last();
-    deprecated
-      .get('.graphiql-markdown-description')
-      .should('contain.text', 'This field is an example of a deprecated field');
-    deprecated
-      .get('.graphiql-markdown-deprecation')
-      .should(
+    // Select query type
+    cy.get('.graphiql-doc-explorer-type-name').first().click();
+
+    // Show deprecated fields
+    cy.contains('Show Deprecated Fields').click();
+
+    // Assert that title is shown
+    cy.get('.graphiql-doc-explorer-section-title').contains(
+      'Deprecated Fields',
+    );
+
+    // Assert that the deprecated field is shown correctly
+    cy.get('.graphiql-doc-explorer-field-name')
+      .contains('deprecatedField')
+      .closest('.graphiql-doc-explorer-item')
+      .should('contain.text', 'This field is an example of a deprecated field')
+      .and(
         'contain.html',
         '<p>No longer in use, try <code>test</code> instead.</p>',
       );
@@ -101,13 +105,11 @@ if (!version.includes('15.5')) {
 
 describeOrSkip('GraphQL DocExplorer - deprecated arguments', () => {
   it('should show deprecated arguments category title', () => {
-    cy.get('#doc-fields .doc-category-item a.graphiql-doc-explorer-field-name')
-      .last()
-      .click();
-    cy.get('#doc-deprecated-args>.doc-category-title')
-      .last()
-      .should('have.text', 'deprecated arguments');
-    cy.get('.show-btn').click();
+    cy.get('.graphiql-doc-explorer-field-name').contains('hasArgs').click();
+    cy.contains('Show Deprecated Arguments').click();
+    cy.get('.graphiql-doc-explorer-section-title').contains(
+      'Deprecated Arguments',
+    );
     cy.get('.graphiql-markdown-deprecation').should(
       'have.text',
       'deprecated argument\n',
