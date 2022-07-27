af500ecb3d4ec906cf97c768e4b1ce120ab79219
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2022-07-27 13:42:03 +0200

    [redesign] docs (#2588)
    * add icons
    
    * add `Button` component to `@graphiql/react`
    
    * add explorer section component
    
    * redesign `SchemaDocumentation` component
    
    * redesign `TypeDocumentation` component
    
    * redesign `FieldDocumentation` component
    
    * redesign `DocExplorer` component
    
    * extend changeset message

--- packages/graphiql-react/src/explorer/components/__tests__/doc-explorer.spec.tsx
+++ packages/graphiql-react/src/explorer/components/__tests__/doc-explorer.spec.tsx
@@ -1,25 +1,23 @@
 import { render } from '@testing-library/react';
+import { GraphQLSchema } from 'graphql';
 
 import { SchemaContext, SchemaContextType } from '../../../schema';
 import { ExplorerContextProvider } from '../../context';
 import { DocExplorer } from '../doc-explorer';
-import { ExampleSchema } from './example-schema';
 
 const defaultSchemaContext: SchemaContextType = {
   fetchError: null,
   isFetching: false,
-  schema: ExampleSchema,
+  schema: new GraphQLSchema({ description: 'GraphQL Schema for testing' }),
   setFetchError() {},
   setSchema() {},
   validationErrors: null,
 };
 
-function DocExplorerWithContext(
-  props: React.ComponentProps<typeof DocExplorer>,
-) {
+function DocExplorerWithContext() {
   return (
     <ExplorerContextProvider>
-      <DocExplorer {...props} />
+      <DocExplorer />
     </ExplorerContextProvider>
   );
 }
@@ -45,9 +43,9 @@ describe('DocExplorer', () => {
         <DocExplorerWithContext />
       </SchemaContext.Provider>,
     );
-    const error = container.querySelectorAll('.error-container');
+    const error = container.querySelectorAll('.graphiql-doc-explorer-error');
     expect(error).toHaveLength(1);
-    expect(error[0]).toHaveTextContent('No Schema Available');
+    expect(error[0]).toHaveTextContent('No GraphQL schema available');
   });
   it('renders with schema', () => {
     const { container } = render(
@@ -55,7 +53,7 @@ describe('DocExplorer', () => {
         <DocExplorerWithContext />,
       </SchemaContext.Provider>,
     );
-    const error = container.querySelectorAll('.error-container');
+    const error = container.querySelectorAll('.graphiql-doc-explorer-error');
     expect(error).toHaveLength(0);
     expect(
       container.querySelector('.graphiql-markdown-description'),
