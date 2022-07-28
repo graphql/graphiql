5fb9680b4a5433c27771206f24a75b231e41fbb1
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2022-07-28 13:52:33 +0200

    [redesign] add dark mode (#2596)
    * add `ButtonGroup` component to `@graphiql/react`
    
    * dark mode

--- packages/graphiql/__mocks__/@graphiql/react.tsx
+++ packages/graphiql/__mocks__/@graphiql/react.tsx
@@ -15,6 +15,7 @@ export {
   Argument,
   ArgumentIcon,
   Button,
+  ButtonGroup,
   ChevronDownIcon,
   ChevronLeftIcon,
   ChevronUpIcon,
@@ -89,6 +90,7 @@ export {
   usePrettifyEditors,
   useSchemaContext,
   useStorageContext,
+  useTheme,
 } from '@graphiql/react';
 
 export type {
