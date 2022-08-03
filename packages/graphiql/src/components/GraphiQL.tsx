29d1d44bfaea9a9b49e288cd7d846af4552accdf
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2022-08-04 00:17:08 +0200

    remove legacy exports

--- packages/graphiql/src/components/GraphiQL.tsx
+++ packages/graphiql/src/components/GraphiQL.tsx
@@ -346,7 +346,7 @@ export function GraphiQL({
               <ExplorerContextProvider
                 isVisible={docExplorerOpen}
                 onToggleVisibility={onToggleDocs}>
-                <GraphiQLWithContext {...props} />
+                <GraphiQLInterface {...props} />
               </ExplorerContextProvider>
             </ExecutionContextProvider>
           </SchemaContextProvider>
@@ -360,7 +360,7 @@ GraphiQL.Logo = GraphiQLLogo;
 GraphiQL.Toolbar = GraphiQLToolbar;
 GraphiQL.Footer = GraphiQLFooter;
 
-type GraphiQLWithContextProviderProps = Omit<
+export type GraphiQLInterfaceProps = Omit<
   GraphiQLProps,
   | 'dangerouslyAssumeSchemaIsValid'
   | 'defaultQuery'
@@ -384,7 +384,7 @@ type GraphiQLWithContextProviderProps = Omit<
   | 'variables'
 >;
 
-function GraphiQLWithContext(props: GraphiQLWithContextProviderProps) {
+export function GraphiQLInterface(props: GraphiQLInterfaceProps) {
   const editorContext = useEditorContext({ nonNull: true });
   const executionContext = useExecutionContext({ nonNull: true });
   const explorerContext = useExplorerContext();
--- packages/graphiql/src/index.tsx
+++ packages/graphiql/src/index.tsx
@@ -5,90 +5,18 @@
  *  LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
-
 /**
  * GraphiQL
  */
 
-import { GraphiQL } from './components/GraphiQL';
-export { GraphiQL };
+import { GraphiQL, GraphiQLInterface } from './components/GraphiQL';
+export { GraphiQL, GraphiQLInterface };
 export default GraphiQL;
 
 /**
  * Definitions
  */
-export type { GraphiQLProps } from './components/GraphiQL';
-
 export type {
-  Fetcher,
-  FetcherOpts,
-  FetcherParams,
-  FetcherResult,
-  FetcherReturnType,
-  Observable,
-  Storage,
-  SyncFetcherResult,
-} from '@graphiql/toolkit';
-
-/**
- * Legacy exports
- */
-import {
-  onHasCompletion as _onHasCompletion,
-  QueryEditor as OrigQueryEditor,
-  VariableEditor as OrigVariableEditor,
-} from '@graphiql/react';
-import {
-  fillLeafs as _fillLeafs,
-  getSelectedOperationName as _getSelectedOperationName,
-  mergeAst as _mergeAst,
-} from '@graphiql/toolkit';
-
-export const onHasCompletion: typeof _onHasCompletion = function onHasCompletion(
-  ...args
-) {
-  console.warn(
-    'Importing `onHasCompletion` from `graphiql` is deprecated and will be removed in the next major version. Please switch to importing the `onHasCompletion` function provided by the `@graphiql/react` package.',
-  );
-  return _onHasCompletion(...args);
-};
-
-export const QueryEditor: typeof OrigQueryEditor = function QueryEditor(props) {
-  console.warn(
-    'Importing `QueryEditor` from `graphiql` is deprecated and will be removed in the next major version. Please switch to importing the `QueryEditor` component provided by the `@graphiql/react` package.',
-  );
-  return <OrigQueryEditor {...props} />;
-};
-
-export const VariableEditor: typeof OrigVariableEditor = function VariableEditor(
-  props,
-) {
-  console.warn(
-    'Importing `VariableEditor` from `graphiql` is deprecated and will be removed in the next major version. Please switch to importing the `VariableEditor` component provided by the `@graphiql/react` package.',
-  );
-  return <OrigVariableEditor {...props} />;
-};
-
-export const fillLeafs: typeof _fillLeafs = function fillLeafs(...args) {
-  console.warn(
-    'Importing `fillLeafs` from `graphiql` is deprecated and will be removed in the next major version. Please switch to importing the `fillLeafs` function provided by the `@graphiql/toolkit` package.',
-  );
-  return _fillLeafs(...args);
-};
-
-export const getSelectedOperationName: typeof _getSelectedOperationName = function getSelectedOperationName(
-  ...args
-) {
-  console.warn(
-    'Importing `getSelectedOperationName` from `graphiql` is deprecated and will be removed in the next major version. Please switch to importing the `getSelectedOperationName` function provided by the `@graphiql/toolkit` package.',
-  );
-  return _getSelectedOperationName(...args);
-};
-
-export const mergeAst: typeof _mergeAst = function mergeAst(...args) {
-  console.warn(
-    'Importing `mergeAst` from `graphiql` is deprecated and will be removed in the next major version. Please switch to importing the `mergeAst` function provided by the `@graphiql/toolkit` package.',
-  );
-  return _mergeAst(...args);
-};
+  GraphiQLProps,
+  GraphiQLInterfaceProps,
+} from './components/GraphiQL';
