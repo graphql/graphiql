5b88e86b9be4925133a3deeb8b60d99f58c8c413
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2022-08-04 00:05:46 +0200

    refactor outer component to be a class component

--- packages/graphiql/src/components/GraphiQL.tsx
+++ packages/graphiql/src/components/GraphiQL.tsx
@@ -294,22 +294,7 @@ export type GraphiQLProps = {
  *
  * @see https://github.com/graphql/graphiql#usage
  */
-export class GraphiQL extends React.Component<GraphiQLProps> {
-  constructor(props: GraphiQLProps) {
-    super(props);
-  }
-
-  render() {
-    return <GraphiQLProviders {...this.props} />;
-  }
-
-  // Export main windows/panes to be used separately if desired.
-  static Logo = GraphiQLLogo;
-  static Toolbar = GraphiQLToolbar;
-  static Footer = GraphiQLFooter;
-}
-
-function GraphiQLProviders({
+export function GraphiQL({
   dangerouslyAssumeSchemaIsValid,
   docExplorerOpen,
   externalFragments,
@@ -319,12 +304,12 @@ function GraphiQLProviders({
   maxHistoryLength,
   onSchemaChange,
   onTabChange,
-  onToggleHistory,
   onToggleDocs,
-  storage,
+  onToggleHistory,
   schema,
   schemaDescription,
   shouldPersistHeaders,
+  storage,
   validationRules,
   ...props
 }: GraphiQLProps) {
@@ -370,6 +355,10 @@ function GraphiQLProviders({
     </StorageContextProvider>
   );
 }
+// Export main windows/panes to be used separately if desired.
+GraphiQL.Logo = GraphiQLLogo;
+GraphiQL.Toolbar = GraphiQLToolbar;
+GraphiQL.Footer = GraphiQLFooter;
 
 // Add a select-option input to the Toolbar.
 // GraphiQL.Select = ToolbarSelect;
