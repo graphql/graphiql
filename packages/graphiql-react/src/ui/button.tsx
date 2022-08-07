46b94a0577f1736759591016c200812f2392ec85
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2022-08-07 13:08:54 +0200

    forward refs and props for UI components (#2628)

--- packages/graphiql-react/src/ui/button.tsx
+++ packages/graphiql-react/src/ui/button.tsx
@@ -1,31 +1,38 @@
+import { forwardRef } from 'react';
 import { compose } from '../utility/compose';
 
 import './button.css';
 
-export function UnStyledButton(props: JSX.IntrinsicElements['button']) {
-  return (
-    <button
-      {...props}
-      className={compose('graphiql-un-styled', props.className)}
-    />
-  );
-}
+export const UnStyledButton = forwardRef<
+  HTMLButtonElement,
+  JSX.IntrinsicElements['button']
+>((props, ref) => (
+  <button
+    {...props}
+    ref={ref}
+    className={compose('graphiql-un-styled', props.className)}
+  />
+));
+UnStyledButton.displayName = 'UnStyledButton';
 
 type ButtonProps = { state?: 'success' | 'error' };
 
-export function Button(props: ButtonProps & JSX.IntrinsicElements['button']) {
-  return (
-    <button
-      {...props}
-      className={compose(
-        'graphiql-button',
-        props.state === 'success'
-          ? 'graphiql-button-success'
-          : props.state === 'error'
-          ? 'graphiql-button-error'
-          : '',
-        props.className,
-      )}
-    />
-  );
-}
+export const Button = forwardRef<
+  HTMLButtonElement,
+  ButtonProps & JSX.IntrinsicElements['button']
+>((props, ref) => (
+  <button
+    {...props}
+    ref={ref}
+    className={compose(
+      'graphiql-button',
+      props.state === 'success'
+        ? 'graphiql-button-success'
+        : props.state === 'error'
+        ? 'graphiql-button-error'
+        : '',
+      props.className,
+    )}
+  />
+));
+Button.displayName = 'Button';
--- packages/graphiql-react/src/ui/dialog.tsx
+++ packages/graphiql-react/src/ui/dialog.tsx
@@ -1,25 +1,31 @@
 import { Dialog as ReachDialog } from '@reach/dialog';
 import { VisuallyHidden } from '@reach/visually-hidden';
-import { ComponentProps } from 'react';
+import { ComponentProps, forwardRef } from 'react';
 import { CloseIcon } from '../icons';
+import { createComponentGroup } from '../utility/component-group';
 import { compose } from '../utility/compose';
 import { UnStyledButton } from './button';
 
 import './dialog.css';
 
-export function Dialog(props: ComponentProps<typeof ReachDialog>) {
-  return <ReachDialog {...props} />;
-}
+const DialogRoot = forwardRef<
+  HTMLDivElement,
+  ComponentProps<typeof ReachDialog>
+>((props, ref) => <ReachDialog {...props} ref={ref} />);
+DialogRoot.displayName = 'Dialog';
 
-function DialogClose(props: JSX.IntrinsicElements['button']) {
-  return (
-    <UnStyledButton
-      {...props}
-      className={compose('graphiql-dialog-close', props.className)}>
-      <VisuallyHidden>Close dialog</VisuallyHidden>
-      <CloseIcon />
-    </UnStyledButton>
-  );
-}
+const DialogClose = forwardRef<
+  HTMLButtonElement,
+  JSX.IntrinsicElements['button']
+>((props, ref) => (
+  <UnStyledButton
+    {...props}
+    ref={ref}
+    className={compose('graphiql-dialog-close', props.className)}>
+    <VisuallyHidden>Close dialog</VisuallyHidden>
+    <CloseIcon />
+  </UnStyledButton>
+));
+DialogClose.displayName = 'Dialog.Close';
 
-Dialog.Close = DialogClose;
+export const Dialog = createComponentGroup(DialogRoot, { Close: DialogClose });
