b9123f50d98467dae9a2bb05555d14e9040dafad
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2022-08-07 13:08:54 +0200

    forward refs and props for UI components (#2628)

--- packages/graphiql-react/src/ui/dialog.tsx
+++ packages/graphiql-react/src/ui/dialog.tsx
@@ -1,27 +1,33 @@
 import { Dialog as ReachDialog } from '@reach/dialog';
 import { VisuallyHidden } from '@reach/visually-hidden';
-import { ComponentProps } from 'react';
+import { ComponentProps, forwardRef } from 'react';
 import { CloseIcon } from '../icons';
+import { createComponentGroup } from '../utility/component-group';
 import { compose } from '../utility/compose';
 import { UnStyledButton } from './button';
 
 import './dialog.css';
 
-export function Dialog(props: ComponentProps<typeof ReachDialog>) {
-  return <ReachDialog {...props} />;
-}
+const DialogRoot = forwardRef<
+  HTMLDivElement,
+  ComponentProps<typeof ReachDialog>
+>((props, ref) => <ReachDialog {...props} ref={ref} />);
+DialogRoot.displayName = 'Dialog';
 
-function DialogClose(props: JSX.IntrinsicElements['button']) {
-  return (
-    <UnStyledButton
-      {...props}
-      type="button"
-      className={compose('graphiql-dialog-close', props.className)}
-    >
-      <VisuallyHidden>Close dialog</VisuallyHidden>
-      <CloseIcon />
-    </UnStyledButton>
-  );
-}
+const DialogClose = forwardRef<
+  HTMLButtonElement,
+  JSX.IntrinsicElements['button']
+>((props, ref) => (
+  <UnStyledButton
+    {...props}
+    ref={ref}
+    type="button"
+    className={compose('graphiql-dialog-close', props.className)}
+  >
+    <VisuallyHidden>Close dialog</VisuallyHidden>
+    <CloseIcon />
+  </UnStyledButton>
+));
+DialogClose.displayName = 'Dialog.Close';
 
-Dialog.Close = DialogClose;
+export const Dialog = createComponentGroup(DialogRoot, { Close: DialogClose });
