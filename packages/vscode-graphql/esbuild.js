810f39183cf17a26e82e818785948877fd3977d6
Rikki Schulte <rikki.schulte@gmail.com>
2022-08-12 00:07:04 +0200

    Revert "Upgrade to ts-node 10.x (#2658)"
    This reverts commit 1e03b5600b7678c5985b8223c551ff6f721540d6.

--- packages/vscode-graphql/esbuild.js
+++ packages/vscode-graphql/esbuild.js
@@ -11,11 +11,7 @@ build({
   minify: arg === '--minify',
   platform: 'node',
   outdir: 'out/',
-  external: [
-    'vscode',
-    '@cspotcode/source-map-support', // workaround for https://github.com/cspotcode/node-source-map-support/issues/43
-    './transpilers/swc.js', // suppresses warning via ts-node, but we won't be using it, so we can exclude it
-  ],
+  external: ['vscode'],
   format: 'cjs',
   sourcemap: true,
   watch: isWatchMode,
