86b7a92aedd4b183b72e725f6147426c37c7c348
Samuel Imolorhe <samuelimolo4real@gmail.com>
2022-08-29 00:30:34 +0200

    added typings for svg to fix tsc build

---
+++ packages/graphiql-react/src/custom.d.ts
@@ -0,0 +1,6 @@
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
