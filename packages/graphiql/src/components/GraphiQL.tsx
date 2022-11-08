e1794e790df5666fb0300985859df261f0b0dad3
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-11-08 15:51:03 +0100

    [graphiql] remove useless ternary

--- packages/graphiql/src/components/GraphiQL.tsx
+++ packages/graphiql/src/components/GraphiQL.tsx
@@ -326,33 +326,31 @@ export function GraphiQLInterface(props: GraphiQLInterfaceProps) {
     <div data-testid="graphiql-container" className="graphiql-container">
       <div className="graphiql-sidebar">
         <div className="graphiql-sidebar-section">
-          {pluginContext
-            ? pluginContext?.plugins.map(plugin => {
-                const isVisible = plugin === pluginContext.visiblePlugin;
-                const label = `${isVisible ? 'Hide' : 'Show'} ${plugin.title}`;
-                const Icon = plugin.icon;
-                return (
-                  <Tooltip key={plugin.title} label={label}>
-                    <UnStyledButton
-                      type="button"
-                      className={isVisible ? 'active' : ''}
-                      onClick={() => {
-                        if (isVisible) {
-                          pluginContext.setVisiblePlugin(null);
-                          pluginResize.setHiddenElement('first');
-                        } else {
-                          pluginContext.setVisiblePlugin(plugin);
-                          pluginResize.setHiddenElement(null);
-                        }
-                      }}
-                      aria-label={label}
-                    >
-                      <Icon aria-hidden="true" />
-                    </UnStyledButton>
-                  </Tooltip>
-                );
-              })
-            : null}
+          {pluginContext?.plugins.map(plugin => {
+            const isVisible = plugin === pluginContext.visiblePlugin;
+            const label = `${isVisible ? 'Hide' : 'Show'} ${plugin.title}`;
+            const Icon = plugin.icon;
+            return (
+              <Tooltip key={plugin.title} label={label}>
+                <UnStyledButton
+                  type="button"
+                  className={isVisible ? 'active' : ''}
+                  onClick={() => {
+                    if (isVisible) {
+                      pluginContext.setVisiblePlugin(null);
+                      pluginResize.setHiddenElement('first');
+                    } else {
+                      pluginContext.setVisiblePlugin(plugin);
+                      pluginResize.setHiddenElement(null);
+                    }
+                  }}
+                  aria-label={label}
+                >
+                  <Icon aria-hidden="true" />
+                </UnStyledButton>
+              </Tooltip>
+            );
+          })}
         </div>
         <div className="graphiql-sidebar-section">
           <Tooltip label="Re-fetch GraphQL schema">
