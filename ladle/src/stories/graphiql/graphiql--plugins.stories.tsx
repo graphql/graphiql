ce3dca4474ee3d906630fee65a9f331315781ddc
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2022-11-13 19:47:36 -0600

    test commit3 for diagnosing cspell/ladle issues

--- ladle/src/stories/graphiql/graphiql--plugins.stories.tsx
+++ ladle/src/stories/graphiql/graphiql--plugins.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { createGraphiQLFetcher } from '../../../packages/graphiql-toolkit';
-import { GraphiQL } from '../../../packages/graphiql';
-import { useExplorerPlugin } from '../../../packages/graphiql-plugin-explorer';
+import { createGraphiQLFetcher } from '../../../../packages/graphiql-toolkit';
+import { GraphiQL } from '../../../../packages/graphiql';
+import { useExplorerPlugin } from '../../../../packages/graphiql-plugin-explorer';
 
 const fetcher = createGraphiQLFetcher({
   url: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
