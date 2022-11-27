79ae4f31e225be183fe4233589b8a3124e438353
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-11-27 15:31:05 +0100

    fix unit tests

--- packages/codemirror-graphql/src/__tests__/hint-test.ts
+++ packages/codemirror-graphql/src/__tests__/hint-test.ts
@@ -20,7 +20,8 @@ import {
   __Schema,
   __Type,
 } from 'graphql';
-import { GraphQLHintOptions, IHint, IHints } from '../hint';
+import '../hint';
+import type { GraphQLHintOptions, IHint, IHints } from '../hint';
 import '../mode';
 import {
   TestEnum,
