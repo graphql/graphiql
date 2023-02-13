450422892827e5c4756819cfa68a093db215f640
CaiYiLiang <cherryliang0112@gmail.com>
2023-02-13 18:07:26 +1300

    feat: add createLocalStorage

---
+++ packages/graphiql-toolkit/src/storage/custom.ts
@@ -0,0 +1,34 @@
+/**
+ * This function enable a custom namespace for localStorage
+ *
+ */
+
+import { Storage } from './base';
+
+export function createLocalStorage(namespace: string): Storage {
+  const storage = {
+    setItem: (key, value) => localStorage.setItem(`${namespace}:${key}`, value),
+    getItem: key => localStorage.getItem(`${namespace}:${key}`),
+    removeItem: key => localStorage.removeItem(`${namespace}:${key}`),
+    get length() {
+      let keys = 0;
+      for (const key in window.localStorage) {
+        if (key.indexOf(`${namespace}:`) === 0) {
+          keys += 1;
+        }
+      }
+      return keys;
+    },
+
+    clear: () => {
+      // We only want to clear the namespaced items
+      for (const key in window.localStorage) {
+        if (key.indexOf(`${namespace}:`) === 0) {
+          window.localStorage.removeItem(key);
+        }
+      }
+    },
+  } as Storage;
+
+  return storage;
+}
--- packages/graphiql-toolkit/src/storage/index.ts
+++ packages/graphiql-toolkit/src/storage/index.ts
@@ -1,3 +1,4 @@
 export * from './base';
 export * from './history';
 export * from './query';
+export * from './custom';
