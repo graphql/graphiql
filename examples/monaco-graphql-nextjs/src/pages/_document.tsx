3cf6881e8bfd170a6440582b9322c62b5afd962f
Rikki Schulte <rikki.schulte@gmail.com>
2023-02-24 23:16:53 +0100

    add an example with next.js and monaco-graphql

---
+++ examples/monaco-graphql-nextjs/src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
