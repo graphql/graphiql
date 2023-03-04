4e5f85d6275a827f2628a7880338e22cef09dbbc
Rikki Schulte <rikki.schulte@gmail.com>
2023-03-04 11:17:45 +0100

    fix monaco peer deps and linting

--- examples/monaco-graphql-nextjs/src/pages/_app.tsx
+++ examples/monaco-graphql-nextjs/src/pages/_app.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
--- packages/monaco-graphql/src/typings/refs.d.ts
+++ packages/monaco-graphql/src/typings/refs.d.ts
@@ -1,2 +1,1 @@
-/// <reference path='../../../../node_modules/monaco-editor/monaco.d.ts'/>
-/// <reference path='./monaco.d.ts'/>
+/// <reference path='../../node_modules/monaco-editor/monaco.d.ts'/>
