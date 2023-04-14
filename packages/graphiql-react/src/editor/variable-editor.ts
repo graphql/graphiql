4092fe2b8a6d7cd162b7e5545f23e133f023f73a
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-04-15 01:35:51 +0200

    replace rest of `event.keyCode` usages by `event.code`

--- packages/graphiql-react/src/editor/variable-editor.ts
+++ packages/graphiql-react/src/editor/variable-editor.ts
@@ -116,9 +116,9 @@ export function useVariableEditor(
       });
 
       newEditor.on('keyup', (editorInstance, event) => {
-        const { keyCode, key, shiftKey } = event;
-        const isLetter = keyCode >= 65 && keyCode <= 90;
-        const isNumber = keyCode >= 48 && keyCode <= 57;
+        const { code, key, shiftKey } = event;
+        const isLetter = code.startsWith('Key');
+        const isNumber = code.startsWith('Digit');
         if (isLetter || (!shiftKey && isNumber) || key === '_' || key === '"') {
           editorInstance.execCommand('autocomplete');
         }
