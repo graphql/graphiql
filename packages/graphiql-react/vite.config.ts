b8f810ed8e7c50321770a98384e670eb6ecc6c3a
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2023-05-22 12:50:01 +0200

    fix duplicated vite config

--- packages/graphiql-react/vite.config.ts
+++ packages/graphiql-react/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import reactSvgPlugin from 'vite-plugin-react-svg';
+import postCssNestingPlugin from 'postcss-nesting';
 
 export default defineConfig({
   plugins: [
@@ -11,12 +12,18 @@ export default defineConfig({
       titleProp: true,
     }),
   ],
+  css: {
+    postcss: {
+      plugins: [postCssNestingPlugin()],
+    },
+  },
   esbuild: {
     // We use function names for generating readable error messages, so we want
     // them to be preserved when building and minifying.
     keepNames: true,
   },
   build: {
+    sourcemap: true,
     lib: {
       entry: 'src/index.ts',
       fileName: 'graphiql-react',
