92ccc041f1443ad05027230cfe577a443908ab46
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-06-01 23:47:23 +0200

    5️⃣ replace all `ReactDOM.render(...)` by `createRoot(...)` from React 18 (#3185)

--- packages/graphiql-react/src/editor/response-editor.tsx
+++ packages/graphiql-react/src/editor/response-editor.tsx
@@ -103,6 +103,7 @@ export function useResponseEditor(
             );
           }
 
+          // We can't refactor to root.unmount() from because we support React 16/17 too
           if (!infoElements.length) {
             ReactDOM.unmountComponentAtNode(tooltipDiv);
             return null;
