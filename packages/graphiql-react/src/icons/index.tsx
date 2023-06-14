e02b6d31bcca1e4ff4d97a5804cb8287746e2184
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-06-14 19:48:15 +0200

    replace unmaintained `vite-plugin-react-svg` with `vite-plugin-svgr`

--- packages/graphiql-react/src/icons/index.tsx
+++ packages/graphiql-react/src/icons/index.tsx
@@ -29,64 +29,47 @@ import _StarIcon from './star.svg';
 import _StopIcon from './stop.svg';
 import _TypeIcon from './type.svg';
 
-export const ArgumentIcon = generateIcon(_ArgumentIcon, 'argument icon');
-export const ChevronDownIcon = generateIcon(
-  _ChevronDownIcon,
-  'chevron down icon',
-);
-export const ChevronLeftIcon = generateIcon(
-  _ChevronLeftIcon,
-  'chevron left icon',
-);
-export const ChevronUpIcon = generateIcon(_ChevronUpIcon, 'chevron up icon');
-export const CloseIcon = generateIcon(_CloseIcon, 'close icon');
-export const CopyIcon = generateIcon(_CopyIcon, 'copy icon');
-export const DeprecatedArgumentIcon = generateIcon(
-  _DeprecatedArgumentIcon,
-  'deprecated argument icon',
-);
-export const DeprecatedEnumValueIcon = generateIcon(
-  _DeprecatedEnumValueIcon,
-  'deprecated enum value icon',
-);
-export const DeprecatedFieldIcon = generateIcon(
-  _DeprecatedFieldIcon,
-  'deprecated field icon',
-);
-export const DirectiveIcon = generateIcon(_DirectiveIcon, 'directive icon');
+export const ArgumentIcon = generateIcon(_ArgumentIcon);
+export const ChevronDownIcon = generateIcon(_ChevronDownIcon);
+export const ChevronLeftIcon = generateIcon(_ChevronLeftIcon);
+export const ChevronUpIcon = generateIcon(_ChevronUpIcon);
+export const CloseIcon = generateIcon(_CloseIcon);
+export const CopyIcon = generateIcon(_CopyIcon);
+export const DeprecatedArgumentIcon = generateIcon(_DeprecatedArgumentIcon);
+export const DeprecatedEnumValueIcon = generateIcon(_DeprecatedEnumValueIcon);
+export const DeprecatedFieldIcon = generateIcon(_DeprecatedFieldIcon);
+export const DirectiveIcon = generateIcon(_DirectiveIcon);
 export const DocsFilledIcon = generateIcon(_DocsFilledIcon, 'filled docs icon');
-export const DocsIcon = generateIcon(_DocsIcon, 'docs icon');
-export const EnumValueIcon = generateIcon(_EnumValueIcon, 'enum value icon');
-export const FieldIcon = generateIcon(_FieldIcon, 'field icon');
-export const HistoryIcon = generateIcon(_HistoryIcon, 'history icon');
-export const ImplementsIcon = generateIcon(_ImplementsIcon, 'implements icon');
-export const KeyboardShortcutIcon = generateIcon(
-  _KeyboardShortcutIcon,
-  'keyboard shortcut icon',
-);
-export const MagnifyingGlassIcon = generateIcon(
-  _MagnifyingGlassIcon,
-  'magnifying glass icon',
-);
-export const MergeIcon = generateIcon(_MergeIcon, 'merge icon');
-export const PenIcon = generateIcon(_PenIcon, 'pen icon');
-export const PlayIcon = generateIcon(_PlayIcon, 'play icon');
-export const PlusIcon = generateIcon(_PlusIcon, 'plus icon');
-export const PrettifyIcon = generateIcon(_PrettifyIcon, 'prettify icon');
-export const ReloadIcon = generateIcon(_ReloadIcon, 'reload icon');
-export const RootTypeIcon = generateIcon(_RootTypeIcon, 'root type icon');
-export const SettingsIcon = generateIcon(_SettingsIcon, 'settings icon');
+export const DocsIcon = generateIcon(_DocsIcon);
+export const EnumValueIcon = generateIcon(_EnumValueIcon);
+export const FieldIcon = generateIcon(_FieldIcon);
+export const HistoryIcon = generateIcon(_HistoryIcon);
+export const ImplementsIcon = generateIcon(_ImplementsIcon);
+export const KeyboardShortcutIcon = generateIcon(_KeyboardShortcutIcon);
+export const MagnifyingGlassIcon = generateIcon(_MagnifyingGlassIcon);
+export const MergeIcon = generateIcon(_MergeIcon);
+export const PenIcon = generateIcon(_PenIcon);
+export const PlayIcon = generateIcon(_PlayIcon);
+export const PlusIcon = generateIcon(_PlusIcon);
+export const PrettifyIcon = generateIcon(_PrettifyIcon);
+export const ReloadIcon = generateIcon(_ReloadIcon);
+export const RootTypeIcon = generateIcon(_RootTypeIcon);
+export const SettingsIcon = generateIcon(_SettingsIcon);
 export const StarFilledIcon = generateIcon(_StarFilledIcon, 'filled star icon');
-export const StarIcon = generateIcon(_StarIcon, 'star icon');
-export const StopIcon = generateIcon(_StopIcon, 'stop icon');
-export const TypeIcon = generateIcon(_TypeIcon, 'type icon');
+export const StarIcon = generateIcon(_StarIcon);
+export const StopIcon = generateIcon(_StopIcon);
+export const TypeIcon = generateIcon(_TypeIcon);
 
-function generateIcon(RawComponent: any, title: string) {
-  const WithTitle = function IconComponent(
-    props: JSX.IntrinsicElements['svg'],
-  ) {
-    return <RawComponent {...props} title={title} />;
-  };
-  Object.defineProperty(WithTitle, 'name', { value: RawComponent.name });
-  return WithTitle;
+function generateIcon(
+  RawComponent: any,
+  title = RawComponent.name
+    // Icon component name starts with `Svg${CamelCaseFilename without .svg}`
+    .replace('Svg', '')
+    // Insert a space before all caps
+    .replaceAll(/([A-Z])/g, ' $1')
+    .trimStart()
+    .toLowerCase() + ' icon',
+) {
+  RawComponent.defaultProps = { title };
+  return RawComponent;
 }
