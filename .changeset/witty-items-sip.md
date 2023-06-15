67bf93a33e98c60ae3a686063a1c47037f88ef49
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-06-15 12:35:37 +0200

    [@graphiql/react]: exclude peer dependencies and dependencies from bundle (#3228)

---
+++ .changeset/witty-items-sip.md
@@ -0,0 +1,5 @@
+---
+'@graphiql/react': patch
+---
+
+exclude peer dependencies and dependencies from bundle
--- packages/graphiql-react/vite.config.ts
+++ packages/graphiql-react/vite.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import reactSvgPlugin from 'vite-plugin-react-svg';
+import svgr from 'vite-plugin-svgr';
 import postCssNestingPlugin from 'postcss-nesting';
+import packageJSON from './package.json';
 
 export default defineConfig({
   plugins: [
     react(),
-    reactSvgPlugin({
-      defaultExport: 'component',
-      expandProps: 'end',
-      titleProp: true,
+    svgr({
+      exportAsDefault: true,
+      svgrOptions: {
+        titleProp: true,
+      },
     }),
   ],
   css: {
@@ -26,11 +28,18 @@ export default defineConfig({
     sourcemap: true,
     lib: {
       entry: 'src/index.ts',
-      fileName: 'graphiql-react',
+      fileName: 'index',
       formats: ['cjs', 'es'],
     },
     rollupOptions: {
-      external: ['graphql', 'react', 'react-dom', 'react/jsx-runtime'],
+      external: [
+        'react/jsx-runtime',
+        // Exclude peer dependencies and dependencies from bundle
+        ...Object.keys(packageJSON.peerDependencies),
+        ...Object.keys(packageJSON.dependencies).filter(
+          dependency => dependency !== 'codemirror',
+        ),
+      ],
       output: {
         chunkFileNames: '[name].[format].js',
       },
