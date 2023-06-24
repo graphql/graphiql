7d30f4693a8d5a366b4f8d030da221786944e4d5
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-06-24 19:53:24 +0200

    more

--- examples/monaco-graphql-nextjs/src/pages/index.tsx
+++ examples/monaco-graphql-nextjs/src/pages/index.tsx
@@ -15,9 +15,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <DynamicEditor />
-      </main>
+      <DynamicEditor />
     </>
   );
 }
