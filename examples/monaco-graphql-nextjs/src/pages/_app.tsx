38efaa8564d849ff9e3dd8b2c6ddb378ce2860c6
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-06-24 19:36:26 +0200

    update deps

--- examples/monaco-graphql-nextjs/src/pages/_app.tsx
+++ examples/monaco-graphql-nextjs/src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
