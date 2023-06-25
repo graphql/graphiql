3ca03597892ad8d4c1c5a8c4b2431f8333dee3c4
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-06-25 02:09:51 +0200

    add tests

---
+++ packages/monaco-graphql/test/monaco-editor.test.ts
@@ -0,0 +1,20 @@
+import { $ } from 'execa';
+
+describe('monaco-editor', () => {
+  it('should include in bundle only graphql/json languages', async () => {
+    const { stdout } =
+      await $`yarn workspace example-monaco-graphql-react-vite build`;
+    const lines = stdout.split('\n');
+    expect(lines[3]).toBe('✓ 1093 modules transformed.');
+    expect(lines[4]).toBe('rendering chunks...');
+    expect(lines[5]).toBe('computing gzip size...');
+    expect(lines[6].startsWith('dist/index.html')).toBe(true);
+    expect(lines[7].startsWith('dist/assets/codicon-')).toBe(true);
+    expect(lines[8].startsWith('dist/assets/index-')).toBe(true);
+    expect(lines[9].startsWith('dist/assets/graphql-')).toBe(true);
+    expect(lines[10].startsWith('dist/assets/jsonMode-')).toBe(true);
+    expect(lines[11].startsWith('dist/assets/graphqlMode-')).toBe(true);
+    expect(lines[12].startsWith('dist/assets/index-')).toBe(true);
+    expect(lines[13].startsWith('✓ built in ')).toBe(true);
+  }, 15_000);
+});
