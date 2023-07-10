5ffd00c2711b1550b53aef8048771499b8fbcbe4
Rikki Schulte <rikki.schulte@gmail.com>
2023-07-10 19:30:43 +0200

    fix: naming

--- examples/graphiql-webpack/src/index.jsx
+++ examples/graphiql-webpack/src/index.jsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client';
 import { GraphiQL } from 'graphiql';
 import { explorerPlugin } from '@graphiql/plugin-explorer';
 import { snippets } from './snippets';
-import { codeExporterPlugin } from '@graphiql/plugin-code-exporter';
+import { exporterPlugin } from '@graphiql/plugin-code-exporter';
 import 'graphiql/graphiql.css';
 import '@graphiql/plugin-explorer/dist/style.css';
 import '@graphiql/plugin-code-exporter/dist/style.css';
@@ -38,8 +38,7 @@ const style = { height: '100vh' };
  * then use the `useMemo` hook
  */
 const explorer = explorerPlugin();
-
-const exporter = codeExporterPlugin({ snippets });
+const exporter = exporterPlugin({ snippets });
 
 const App = () => {
   return (
