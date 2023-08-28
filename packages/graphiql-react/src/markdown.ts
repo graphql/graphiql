ce09e53ed59e4909b5cb0db4ad58e2a5fa7c7d52
Leonard Ehrenfried <mail@leonard.io>
2023-08-28 13:01:27 +0200

    Don't convert single \n to <br>

--- packages/graphiql-react/src/markdown.ts
+++ packages/graphiql-react/src/markdown.ts
@@ -1,6 +1,8 @@
 import MarkdownIt from 'markdown-it';
 
 export const markdown = new MarkdownIt({
-  breaks: true,
+  // we don't want to convert \n to <br> because in markdown a single newline is not a line break
+  // https://github.com/graphql/graphiql/issues/3155
+  breaks: false,
   linkify: true,
 });
