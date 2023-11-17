129666a9a86690bb72226674d40215f24dc5f7ea
Samuel <samuelimolo4real@gmail.com>
2023-11-17 10:15:32 +0100

    Wrap cm6-graphql lint logic in try..catch (#3461)
    * Wrap cm6-graphql lint logic in try..catch
    
    * Create silver-lions-build.md

--- packages/cm6-graphql/src/lint.ts
+++ packages/cm6-graphql/src/lint.ts
@@ -8,44 +8,48 @@ const SEVERITY = ['error', 'warning', 'info'] as const;
 
 export const lint: Extension = linter(
   view => {
-    const schema = getSchema(view.state);
-    if (!schema) {
+    try {
+      const schema = getSchema(view.state);
+      if (!schema) {
+        return [];
+      }
+      const results = getDiagnostics(view.state.doc.toString(), schema);
+
+      return results
+        .map((item): Diagnostic | null => {
+          if (!item.severity || !item.source) {
+            return null;
+          }
+
+          const calculatedFrom = posToOffset(
+            view.state.doc,
+            new Position(item.range.start.line, item.range.start.character),
+          );
+          const from = Math.max(
+            0,
+            Math.min(calculatedFrom, view.state.doc.length),
+          );
+          const calculatedRo = posToOffset(
+            view.state.doc,
+            new Position(item.range.end.line, item.range.end.character - 1),
+          );
+          const to = Math.min(
+            Math.max(from + 1, calculatedRo),
+            view.state.doc.length,
+          );
+          return {
+            from,
+            to: from === to ? to + 1 : to,
+            severity: SEVERITY[item.severity - 1],
+            // source: item.source, // TODO:
+            message: item.message,
+            actions: [], // TODO:
+          };
+        })
+        .filter((_): _ is Diagnostic => Boolean(_));
+    } catch {
       return [];
     }
-    const results = getDiagnostics(view.state.doc.toString(), schema);
-
-    return results
-      .map((item): Diagnostic | null => {
-        if (!item.severity || !item.source) {
-          return null;
-        }
-
-        const calculatedFrom = posToOffset(
-          view.state.doc,
-          new Position(item.range.start.line, item.range.start.character),
-        );
-        const from = Math.max(
-          0,
-          Math.min(calculatedFrom, view.state.doc.length),
-        );
-        const calculatedRo = posToOffset(
-          view.state.doc,
-          new Position(item.range.end.line, item.range.end.character - 1),
-        );
-        const to = Math.min(
-          Math.max(from + 1, calculatedRo),
-          view.state.doc.length,
-        );
-        return {
-          from,
-          to: from === to ? to + 1 : to,
-          severity: SEVERITY[item.severity - 1],
-          // source: item.source, // TODO:
-          message: item.message,
-          actions: [], // TODO:
-        };
-      })
-      .filter((_): _ is Diagnostic => Boolean(_));
   },
   {
     needsRefresh(vu) {
