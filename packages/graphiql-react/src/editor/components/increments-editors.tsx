074061a2a1fd608201dddad869304e7b34a837de
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2024-05-16 17:25:50 -0600

    missing effect dependency

--- packages/graphiql-react/src/editor/components/increments-editors.tsx
+++ packages/graphiql-react/src/editor/components/increments-editors.tsx
@@ -81,7 +81,7 @@ function useIncrementsEditor({
     return () => {
       isActive = false;
     };
-  }, [editorTheme]);
+  }, [editorTheme, increment.payload]);
 
   useSynchronizeOption(editor, 'keyMap', keyMap);
 
