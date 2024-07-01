8849a15b6e80fe1b34e8250e74a56b85ccdb6ac6
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-07-01 11:40:43 +0200

    adjust `@graphiql/plugin-explorer` styles (#3633)

--- packages/graphiql-plugin-explorer/src/vite-env.d.ts
+++ packages/graphiql-plugin-explorer/src/vite-env.d.ts
@@ -1,1 +1,7 @@
+declare module '*.svg' {
+  import { FC, SVGProps } from 'react';
+  const ReactComponent: FC<SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+}
+
 /// <reference types="vite/client" />
