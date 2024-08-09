a8393c79e5cf94680b5989b9f3d7bafc736eac65
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-08-09 15:35:43 +0200

    fix cypress

--- packages/graphiql/test/e2e-server.js
+++ packages/graphiql/test/e2e-server.js
@@ -8,6 +8,7 @@
 /* eslint-disable no-console, import-x/no-extraneous-dependencies */
 import { createServer } from 'node:http';
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import express from 'express';
 import { GraphQLError } from 'graphql';
 import { createHandler } from 'graphql-http/lib/use/express';
@@ -40,6 +41,8 @@ app.post('/graphql-error/graphql', (_req, res, next) => {
 
 // On CI we test the UMD build
 if (process.env.CI === 'true') {
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  // const __dirname = import.meta.dirname; // can be converted to, after Node.js upgrade to v20
   app.use(express.static(path.join(__dirname, '..')));
 } else {
   app.get('/', (req, res) => {
