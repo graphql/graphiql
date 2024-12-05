e17ef54a6f2484f1a9cb4e3c9e0a4b3f586554d7
Roberto Aloi <robertoaloi@meta.com>
2024-12-05 16:44:27 +0100

    Configurable LSP transport

---
+++ packages/vscode-graphql/src/serverStdio/index.ts
@@ -0,0 +1,18 @@
+// this lives in the same monorepo! most errors you see in
+// vscode that aren't highlighting or bracket completion
+// related are coming from our LSP server
+import { startServer } from 'graphql-language-service-server';
+
+// The npm scripts are configured to only build this once before
+// watching the extension, so please restart the extension debugger for changes!
+
+async function start() {
+  try {
+    await startServer({ method: 'stream' });
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+  }
+}
+
+void start();
