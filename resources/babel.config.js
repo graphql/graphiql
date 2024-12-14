439e7a53c9cfe3a6e3e48ce55bacaf7cc9565b82
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-12-15 03:38:56 +0800

    rm babel

--- resources/babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  sourceMaps: true,
-  presets: [
-    require.resolve('@babel/preset-react'),
-  ],
-  plugins: [require.resolve('@babel/plugin-proposal-class-properties')],
-};
