3735b1d9cf1816ec24ee21c479e3cb26fef2a57a
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-05-04 23:16:51 +0200

    fix unit tests

--- packages/graphiql/test/schema.js
+++ packages/graphiql/test/schema.js
@@ -9,8 +9,8 @@
 import { createRequire } from 'node:module';
 import { fileURLToPath } from 'node:url';
 
-const __filename = fileURLToPath(import.meta.url);
-const require = createRequire(__filename);
+const filename = fileURLToPath(import.meta.url);
+const require = createRequire(filename);
 
 const {
   GraphQLSchema,
