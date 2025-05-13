874b0fdb805c6519ba4033db2fc0ecb4feda0ef3
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-05-13 04:00:04 +0200

    upd

--- packages/graphiql-plugin-doc-explorer/src/components/__tests__/test-utils.ts
+++ packages/graphiql-plugin-doc-explorer/src/components/__tests__/test-utils.ts
@@ -1,13 +1,5 @@
 'use no memo';
-import { useRef } from 'react';
 import { GraphQLNamedType, GraphQLType } from 'graphql';
-import { createDocExplorerStore, DocExplorerNavStackItem } from '../../context';
-
-export function useMockDocExplorerContextValue(
-  navStackItem: DocExplorerNavStackItem,
-) {
-  return useRef(createDocExplorerStore(navStackItem));
-}
 
 export function unwrapType(type: GraphQLType): GraphQLNamedType {
   return 'ofType' in type ? unwrapType(type.ofType) : type;
