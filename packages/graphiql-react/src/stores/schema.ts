639bc4c3f0620a03e627e63f0df95346493ae961
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-05-15 11:37:23 +0200

    upd

--- packages/graphiql-react/src/stores/schema.ts
+++ packages/graphiql-react/src/stores/schema.ts
@@ -17,7 +17,7 @@ import {
 import { Dispatch, FC, ReactElement, ReactNode, useEffect } from 'react';
 import { createStore } from 'zustand';
 import { useEditorStore } from './editor';
-import type { SchemaReference } from 'codemirror-graphql/utils/SchemaReference';
+import { SchemaReference } from '../editor/types';
 import { createBoundedUseStore } from '../utility';
 import { executionStore, useExecutionStore } from './execution';
 
