edeb8399ba263e99f78eebab8d1995b6cd8ad25f
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-05-15 21:20:09 +0200

    constants

--- packages/graphiql-react/src/constants.ts
+++ packages/graphiql-react/src/constants.ts
@@ -1,6 +1,49 @@
 import { initializeMode } from 'monaco-graphql/esm/initializeMode.js';
 import { editor, Uri } from 'monaco-editor';
-import { DEFAULT_QUERY } from './editor/context';
+
+export const KEY_MAP = Object.freeze({
+  prettify: ['Shift-Ctrl-P'],
+  mergeFragments: ['Shift-Ctrl-M'],
+  runQuery: ['Ctrl-Enter', 'Cmd-Enter'],
+  autoComplete: ['Ctrl-Space'],
+  copyQuery: ['Shift-Ctrl-C'],
+  refetchSchema: ['Shift-Ctrl-R'],
+  searchInEditor: ['Ctrl-F'],
+  searchInDocs: ['Ctrl-K'],
+} as const);
+
+export const DEFAULT_QUERY = `# Welcome to GraphiQL
+#
+# GraphiQL is an in-browser tool for writing, validating, and testing
+# GraphQL queries.
+#
+# Type queries into this side of the screen, and you will see intelligent
+# typeaheads aware of the current GraphQL type schema and live syntax and
+# validation errors highlighted within the text.
+#
+# GraphQL queries typically start with a "{" character. Lines that start
+# with a # are ignored.
+#
+# An example GraphQL query might look like:
+#
+#     {
+#       field(arg: "value") {
+#         subField
+#       }
+#     }
+#
+# Keyboard shortcuts:
+#
+#   Prettify query:  ${KEY_MAP.prettify[0]} (or press the prettify button)
+#
+#  Merge fragments:  ${KEY_MAP.mergeFragments[0]} (or press the merge button)
+#
+#        Run Query:  ${KEY_MAP.runQuery[0]} (or press the play button)
+#
+#    Auto Complete:  ${KEY_MAP.autoComplete[0]} (or just start typing)
+#
+
+`;
 
 const OPERATIONS_URI = 'operations.graphql';
 const VARIABLES_URI = 'variables.json';
@@ -448,46 +491,3 @@ export const editorThemeLight: editor.IStandaloneThemeData = {
 // 'peekViewResult.matchHighlightBackground': "#FFFFFF00", // Match highlight color in the peek view result list.
 // 'peekViewEditor.matchHighlightBackground': "#FFFFFF00", // Match highlight color in the peek view editor.
 
-export const KEY_MAP = Object.freeze({
-  prettify: ['Shift-Ctrl-P'],
-  mergeFragments: ['Shift-Ctrl-M'],
-  runQuery: ['Ctrl-Enter', 'Cmd-Enter'],
-  autoComplete: ['Ctrl-Space'],
-  copyQuery: ['Shift-Ctrl-C'],
-  refetchSchema: ['Shift-Ctrl-R'],
-  searchInEditor: ['Ctrl-F'],
-  searchInDocs: ['Ctrl-K'],
-} as const);
-
-export const DEFAULT_QUERY = `# Welcome to GraphiQL
-#
-# GraphiQL is an in-browser tool for writing, validating, and testing
-# GraphQL queries.
-#
-# Type queries into this side of the screen, and you will see intelligent
-# typeaheads aware of the current GraphQL type schema and live syntax and
-# validation errors highlighted within the text.
-#
-# GraphQL queries typically start with a "{" character. Lines that start
-# with a # are ignored.
-#
-# An example GraphQL query might look like:
-#
-#     {
-#       field(arg: "value") {
-#         subField
-#       }
-#     }
-#
-# Keyboard shortcuts:
-#
-#   Prettify query:  ${KEY_MAP.prettify[0]} (or press the prettify button)
-#
-#  Merge fragments:  ${KEY_MAP.mergeFragments[0]} (or press the merge button)
-#
-#        Run Query:  ${KEY_MAP.runQuery[0]} (or press the play button)
-#
-#    Auto Complete:  ${KEY_MAP.autoComplete[0]} (or just start typing)
-#
-
-`;
