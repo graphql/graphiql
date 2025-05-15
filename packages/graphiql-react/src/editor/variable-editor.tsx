821a0f12c04549ca84a057a2b80d409b7af6317d
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-05-15 17:40:39 +0200

    cleanup

--- packages/graphiql-react/src/editor/variable-editor.tsx
+++ packages/graphiql-react/src/editor/variable-editor.tsx
@@ -1,22 +1,25 @@
 import { useEffect, useRef } from 'react';
-import { useExecutionStore, useEditorStore } from '../stores';
+import { useExecutionStore, useEditorStore, storageStore } from '../stores';
 import {
-  commonKeys,
+  // commonKeys,
   DEFAULT_EDITOR_THEME,
   DEFAULT_KEY_MAP,
   importCodeMirror,
 } from './common';
-import {
-  useChangeHandler,
-  useCompletion,
-  useKeyMap,
-  mergeQuery,
-  prettifyEditors,
-  useSynchronizeOption,
-} from './hooks';
+// import {
+//   useChangeHandler,
+//   useCompletion,
+//   useKeyMap,
+//   mergeQuery,
+//   prettifyEditors,
+//   useSynchronizeOption,
+// } from './hooks';
 import { WriteableEditorProps, SchemaReference } from './types';
-import { KEY_MAP } from '../constants';
+// import { KEY_MAP } from '../constants';
 import { clsx } from 'clsx';
+import { createEditor } from '../create-editor';
+import { VARIABLES_MODEL } from '../constants';
+import { debounce } from '../utility';
 
 type VariableEditorProps = WriteableEditorProps & {
   /**
@@ -54,88 +57,104 @@ export function VariableEditor({
   readOnly = false,
   isHidden = false,
 }: VariableEditorProps) {
-  const { initialVariables, variableEditor, setVariableEditor } =
-    useEditorStore();
-  const run = useExecutionStore(store => store.run);
+  const {
+    // initialVariables,
+    // variableEditor,
+    setVariableEditor,
+    updateActiveTabValues,
+  } = useEditorStore();
+  // const run = useExecutionStore(store => store.run);
   const ref = useRef<HTMLDivElement>(null!);
+  // useEffect(() => {
+  //   let isActive = true;
+  //
+  //   void importCodeMirrorImports().then(CodeMirror => {
+  //     // Don't continue if the effect has already been cleaned up
+  //     if (!isActive) {
+  //       return;
+  //     }
+  //     const container = ref.current;
+  //     const newEditor = CodeMirror(container, {
+  //       value: initialVariables,
+  //       lineNumbers: true,
+  //       tabSize: 2,
+  //       mode: 'graphql-variables',
+  //       theme: editorTheme,
+  //       autoCloseBrackets: true,
+  //       matchBrackets: true,
+  //       showCursorWhenSelecting: true,
+  //       readOnly: readOnly ? 'nocursor' : false,
+  //       foldGutter: true,
+  //       lint: {
+  //         // @ts-expect-error
+  //         variableToType: undefined,
+  //       },
+  //       hintOptions: {
+  //         closeOnUnfocus: false,
+  //         completeSingle: false,
+  //         container,
+  //         // @ts-expect-error
+  //         variableToType: undefined,
+  //       },
+  //       gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter'],
+  //       extraKeys: commonKeys,
+  //     });
+  //
+  //     function showHint() {
+  //       newEditor.showHint({ completeSingle: false, container });
+  //     }
+  //
+  //     newEditor.addKeyMap({
+  //       'Cmd-Space': showHint,
+  //       'Ctrl-Space': showHint,
+  //       'Alt-Space': showHint,
+  //       'Shift-Space': showHint,
+  //     });
+  //
+  //     newEditor.on('keyup', (editorInstance, event) => {
+  //       const { code, key, shiftKey } = event;
+  //       const isLetter = code.startsWith('Key');
+  //       const isNumber = !shiftKey && code.startsWith('Digit');
+  //       if (isLetter || isNumber || key === '_' || key === '"') {
+  //         editorInstance.execCommand('autocomplete');
+  //       }
+  //     });
+  //
+  //     setVariableEditor(newEditor);
+  //   });
+  //
+  //   return () => {
+  //     isActive = false;
+  //   };
+  // }, [editorTheme, initialVariables, readOnly, setVariableEditor]);
+  //
+  // useSynchronizeOption(variableEditor, 'keyMap', keyMap);
+  //
+  // useChangeHandler(variableEditor, onEdit, STORAGE_KEY, 'variables');
+  //
+  // useCompletion(variableEditor, onClickReference);
+  //
+  // useKeyMap(variableEditor, KEY_MAP.runQuery, run);
+  // useKeyMap(variableEditor, KEY_MAP.prettify, prettifyEditors);
+  // useKeyMap(variableEditor, KEY_MAP.mergeFragments, mergeQuery);
+  //
+  // useEffect(() => {
+  //   if (!isHidden) {
+  //     variableEditor?.refresh();
+  //   }
+  // }, [variableEditor, isHidden]);
+
   useEffect(() => {
-    let isActive = true;
-
-    void importCodeMirrorImports().then(CodeMirror => {
-      // Don't continue if the effect has already been cleaned up
-      if (!isActive) {
-        return;
-      }
-      const container = ref.current;
-      const newEditor = CodeMirror(container, {
-        value: initialVariables,
-        lineNumbers: true,
-        tabSize: 2,
-        mode: 'graphql-variables',
-        theme: editorTheme,
-        autoCloseBrackets: true,
-        matchBrackets: true,
-        showCursorWhenSelecting: true,
-        readOnly: readOnly ? 'nocursor' : false,
-        foldGutter: true,
-        lint: {
-          // @ts-expect-error
-          variableToType: undefined,
-        },
-        hintOptions: {
-          closeOnUnfocus: false,
-          completeSingle: false,
-          container,
-          // @ts-expect-error
-          variableToType: undefined,
-        },
-        gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter'],
-        extraKeys: commonKeys,
-      });
-
-      function showHint() {
-        newEditor.showHint({ completeSingle: false, container });
-      }
-
-      newEditor.addKeyMap({
-        'Cmd-Space': showHint,
-        'Ctrl-Space': showHint,
-        'Alt-Space': showHint,
-        'Shift-Space': showHint,
-      });
-
-      newEditor.on('keyup', (editorInstance, event) => {
-        const { code, key, shiftKey } = event;
-        const isLetter = code.startsWith('Key');
-        const isNumber = !shiftKey && code.startsWith('Digit');
-        if (isLetter || isNumber || key === '_' || key === '"') {
-          editorInstance.execCommand('autocomplete');
-        }
-      });
-
-      setVariableEditor(newEditor);
-    });
-
-    return () => {
-      isActive = false;
-    };
-  }, [editorTheme, initialVariables, readOnly, setVariableEditor]);
-
-  useSynchronizeOption(variableEditor, 'keyMap', keyMap);
-
-  useChangeHandler(variableEditor, onEdit, STORAGE_KEY, 'variables');
-
-  useCompletion(variableEditor, onClickReference);
-
-  useKeyMap(variableEditor, KEY_MAP.runQuery, run);
-  useKeyMap(variableEditor, KEY_MAP.prettify, prettifyEditors);
-  useKeyMap(variableEditor, KEY_MAP.mergeFragments, mergeQuery);
-
-  useEffect(() => {
-    if (!isHidden) {
-      variableEditor?.refresh();
-    }
-  }, [variableEditor, isHidden]);
+    setVariableEditor(createEditor('variables', ref.current));
+    VARIABLES_MODEL.onDidChangeContent(
+      debounce(500, () => {
+        const value = VARIABLES_MODEL.getValue();
+        const { storage } = storageStore.getState();
+        storage.set(STORAGE_KEY, value);
+        updateActiveTabValues({ variables: value });
+      }),
+    );
+  }, []);
 
   return (
     <div className={clsx('graphiql-editor', isHidden && 'hidden')} ref={ref} />
