74a6ed03fd6189dd2ee65324e11f179d555d74b3
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-05-18 15:56:14 +0200

    upd

--- packages/graphiql-react/src/editor/types.ts
+++ packages/graphiql-react/src/editor/types.ts
@@ -1,14 +1,17 @@
 import type { editor } from '../monaco-editor';
+import { HTMLAttributes } from 'react';
 
 export type CodeMirrorEditor = Editor & { options?: any };
 
-export type WriteableEditorProps = {
+export type CommonEditorProps = HTMLAttributes<HTMLDivElement>;
+
+export interface WriteableEditorProps extends CommonEditorProps {
   /**
    * Makes the editor read-only.
    * @default false
    */
   readOnly?: boolean;
-};
+}
 
 export type { SchemaReference } from 'codemirror-graphql/utils/SchemaReference';
 
