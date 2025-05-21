0a274f65264717e5d979455956c5704086350266
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-05-22 01:09:02 +0200

    fix vitest

--- packages/monaco-graphql/test/monaco-editor.test.ts
+++ packages/monaco-graphql/test/monaco-editor.test.ts
@@ -13,28 +13,31 @@ describe('monaco-editor', () => {
       .replaceAll(ANSI_COLOR_REGEX, '')
       .split('\n')
       .slice(2, -1)
-      .join('\n');
+      .join('\n')
+      // To replace two or more spaces and everything until the end of the line
+      // Because kb fails on CI
+      .replaceAll(/\s{2,}.*/gm, '');
     expect(output).toMatchInlineSnapshot(`
       "transforming...
       ✓ 902 modules transformed.
       rendering chunks...
       computing gzip size...
-      dist/index.html                      0.69 kB │ gzip:     0.39 kB
-      dist/workers/graphql.js             61.66 kB
-      dist/assets/codicon.ttf             79.57 kB
-      dist/workers/standalone.js          99.14 kB
-      dist/workers/editor.worker.js      437.43 kB
-      dist/workers/json.worker.js        709.37 kB
-      dist/workers/graphql.worker.js     928.08 kB
-      dist/workers/ts.worker.js       10,652.33 kB
-      dist/assets/index.css              154.00 kB │ gzip:    21.96 kB
-      dist/assets/graphql.js               4.15 kB │ gzip:     1.59 kB
-      dist/assets/typescript.js            9.11 kB │ gzip:     2.95 kB
-      dist/assets/index.js                22.57 kB │ gzip:     5.39 kB
-      dist/assets/tsMode.js               42.21 kB │ gzip:     8.24 kB
-      dist/assets/jsonMode.js             85.51 kB │ gzip:    15.62 kB
-      dist/assets/graphqlMode.js         247.09 kB │ gzip:    43.82 kB
-      dist/index.js                    6,403.40 kB │ gzip: 1,177.66 kB"
+      dist/index.html
+      dist/workers/graphql.js
+      dist/assets/codicon.ttf
+      dist/workers/standalone.js
+      dist/workers/editor.worker.js
+      dist/workers/json.worker.js
+      dist/workers/graphql.worker.js
+      dist/workers/ts.worker.js
+      dist/assets/index.css
+      dist/assets/graphql.js
+      dist/assets/typescript.js
+      dist/assets/index.js
+      dist/assets/tsMode.js
+      dist/assets/jsonMode.js
+      dist/assets/graphqlMode.js
+      dist/index.js"
     `);
   }, 40_000);
 });
