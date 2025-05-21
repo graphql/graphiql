383f1833f0ac72d37e0ab8e41fe3a7a94cd94b9c
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-05-21 15:12:28 +0200

    vite example

--- examples/graphiql-vite/src/index.jsx
+++ examples/graphiql-vite/src/index.jsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './index.css';
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('graphiql'));
 root.render(<App />);
---
+++ examples/graphiql-vite/vite.config.mts
@@ -0,0 +1,5 @@
+import react from '@vitejs/plugin-react';
+
+export default {
+  plugins: [react()],
+};
