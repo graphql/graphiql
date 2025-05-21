a4326542d33e9225f66ea6d4f1215bf940620f42
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-05-21 13:57:48 +0200

    upd

--- packages/graphiql/src/index.ts
+++ packages/graphiql/src/index.ts
@@ -1,14 +1,11 @@
 'use no memo';
 
 /**
- *  Copyright (c) 2021 GraphQL Contributors.
+ *  Copyright (c) 2025 GraphQL Contributors.
  *
  *  This source code is licensed under the MIT license found in the
  *  LICENSE file in the root directory of this source tree.
  */
 import './style.css';
 
-/**
- * Definitions
- */
 export { GraphiQL } from './GraphiQL';
