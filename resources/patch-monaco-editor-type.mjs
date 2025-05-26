6b0c6e800277bc32277b8881d5c347bd1ce03190
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-05-27 01:25:54 +0200

    upd

--- resources/patch-monaco-editor-type.mjs
+++ resources/patch-monaco-editor-type.mjs
@@ -5,13 +5,11 @@ const filePath = path.resolve(
   './packages/monaco-graphql/esm/monaco-editor.d.ts',
 );
 
-const newContent = fs.readFileSync(filePath, 'utf8');
+const newContent = fs
+  .readFileSync(filePath, 'utf8')
+  .replace('/esm/vs/editor/edcore.main.js', '');
 
-fs.writeFileSync(
-  filePath,
-  newContent.replace('/esm/vs/editor/edcore.main.js', ''),
-  'utf8',
-);
+fs.writeFileSync(filePath, newContent);
 
 console.log(
   `[patch-monaco-editor-type] Updated "${path.relative(process.cwd(), filePath)}"`,
