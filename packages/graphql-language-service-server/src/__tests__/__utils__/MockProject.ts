5203f4ddcd5b14b197ac06a013cf7d0124166948
Benjie Gillam <benjie@jemjie.com>
2025-05-31 18:07:05 +0100

    This module doesn't exist any more

--- packages/graphql-language-service-server/src/__tests__/__utils__/MockProject.ts
+++ packages/graphql-language-service-server/src/__tests__/__utils__/MockProject.ts
@@ -24,7 +24,7 @@ const modules = [
   'json-parse-even-better-errors',
   'lines-and-columns',
   '@babel/code-frame',
-  '@babel/highlight',
+  // '@babel/highlight',
   // these i think are just required by jest when you console log from a test
   'jest-message-util',
   'stack-utils',
