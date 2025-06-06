71d89194b22de18fb6104eccadd1315ab2de2420
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-06-06 23:11:24 +0200

    polish

--- packages/graphiql/cypress/support/commands.ts
+++ packages/graphiql/cypress/support/commands.ts
@@ -50,31 +50,26 @@ declare namespace Cypress {
   }
 }
 
-// @ts-expect-error -- fixme
 Cypress.Commands.add('dataCy', value => {
-  return cy.get(`[data-cy="${value}"]`);
+  cy.get(`[data-cy="${value}"]`);
 });
 
-// @ts-expect-error -- fixme
 Cypress.Commands.add('clickExecuteQuery', () => {
-  return cy.get('.graphiql-execute-button').click();
+  cy.get('.graphiql-execute-button').click();
 });
 
-// @ts-expect-error -- fixme
 Cypress.Commands.add('clickPrettify', () => {
-  return cy.get('[aria-label="Prettify query (Shift-Ctrl-P)"]').click();
+  cy.get('[aria-label="Prettify query (Shift-Ctrl-P)"]').click();
 });
 
-// @ts-expect-error -- fixme
 Cypress.Commands.add('visitWithOp', ({ query, variables, variablesString }) => {
   let url = `/?query=${encodeURIComponent(query)}`;
   if (variables || variablesString) {
     url += `&variables=${encodeURIComponent(
-      // @ts-expect-error -- fixme
       JSON.stringify(variables, null, 2) || variablesString,
     )}`;
   }
-  return cy.visit(url);
+  cy.visit(url);
 });
 
 Cypress.Commands.add(
