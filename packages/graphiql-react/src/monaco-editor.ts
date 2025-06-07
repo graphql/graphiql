dbad5e3dbe26a35cb41f456b98f5cea06c023f9c
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-06-08 01:22:53 +0200

    try

--- packages/graphiql-react/src/monaco-editor.ts
+++ packages/graphiql-react/src/monaco-editor.ts
@@ -1,7 +1,1 @@
-/**
- * Can't use `monaco-graphql/esm/monaco-editor` due error in esm.sh example:
- * Uncaught TypeError: Cannot read properties of undefined (reading 'jsonDefaults')
- */
-
-// eslint-disable-next-line @typescript-eslint/no-restricted-imports
-export * from 'monaco-editor';
+export * from 'monaco-graphql/esm/monaco-editor';
