e7c37f1a20648f70295a2d6e390149c6525d192d
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-06-10 23:32:34 +0200

    more types check

--- packages/graphiql-react/src/index.ts
+++ packages/graphiql-react/src/index.ts
@@ -7,6 +7,6 @@ export type { TabsState } from './utility/tabs';
 export * from './icons';
 export * from './components';
 
-export type { EditorProps, SchemaReference, AllSlices } from './types';
+export type { EditorProps, SchemaReference, SlicesWithActions } from './types';
 export type { GraphiQLPlugin } from './stores/plugin';
 export { KEY_MAP, formatShortcutForOS, isMacOs } from './constants';
