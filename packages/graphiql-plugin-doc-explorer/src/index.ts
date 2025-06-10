2ab826da4979af0265b0ea9e0ca405c052bd26f6
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-06-10 14:47:41 +0200

    upd

---
+++ packages/graphiql-plugin-doc-explorer/src/index.ts
@@ -0,0 +1,14 @@
+export * from './components';
+
+export {
+  DocExplorerStore,
+  useDocExplorer,
+  useDocExplorerActions,
+  DOC_EXPLORER_PLUGIN
+} from './context';
+
+export type {
+  DocExplorerFieldDef,
+  DocExplorerNavStack,
+  DocExplorerNavStackItem,
+} from './context';
