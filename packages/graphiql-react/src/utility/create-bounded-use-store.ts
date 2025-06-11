a8b7b66a54a2cc793ec333a353f9e603cd1be750
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-06-11 22:49:04 +0200

    polish

--- packages/graphiql-react/src/utility/create-bounded-use-store.ts
+++ packages/graphiql-react/src/utility/create-bounded-use-store.ts
@@ -1,8 +1,11 @@
 import { ExtractState, StoreApi, useStore } from 'zustand';
+import { useShallow } from 'zustand/shallow';
 
 // https://zustand.docs.pmnd.rs/guides/typescript#bounded-usestore-hook-for-vanilla-stores
-export const createBoundedUseStore = (store => selector =>
-  useStore(store, selector)) as <S extends StoreApi<unknown>>(
+export const createBoundedUseStore = (store => selector => {
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- selector can be `undefined`
+  return useStore(store, selector && useShallow(selector));
+}) as <S extends StoreApi<unknown>>(
   store: S,
 ) => {
   (): ExtractState<S>;
