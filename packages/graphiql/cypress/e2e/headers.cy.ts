eeefee74a1223dbfbdabcef8b00426bd09bd187c
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-06-15 20:17:10 +0200

    prettier

--- packages/graphiql/cypress/e2e/headers.cy.ts
+++ packages/graphiql/cypress/e2e/headers.cy.ts
@@ -3,9 +3,7 @@ const DEFAULT_HEADERS = '{"foo":2}';
 describe('Headers', () => {
   describe('`defaultHeaders`', () => {
     it('should have default headers while open new tabs', () => {
-      cy.visit(
-        `/?defaultQuery={test}&defaultHeaders=${DEFAULT_HEADERS}`,
-      );
+      cy.visit(`/?defaultQuery={test}&defaultHeaders=${DEFAULT_HEADERS}`);
       cy.assertHasValues({ query: '{test}', headersString: DEFAULT_HEADERS });
       cy.get('.graphiql-tab-add').click();
       cy.assertHasValues({ query: '', headersString: DEFAULT_HEADERS });
