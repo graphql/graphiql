6b82183f8d88f0a5545eff9779805c5902531c96
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-06-15 20:46:59 +0200

    upd

--- packages/graphiql/cypress/e2e/history.cy.ts
+++ packages/graphiql/cypress/e2e/history.cy.ts
@@ -15,78 +15,78 @@ describe('history', () => {
   });
 
   it('will save history item even when history panel is closed', () => {
-    cy.visit('/?queryy={test}');
+    cy.visit('/?query={test}');
     cy.clickExecuteQuery();
     cy.get('button[aria-label="Show History"]').click();
     cy.get('ul.graphiql-history-items').should('have.length', 1);
   });
 
   it('will save history item even when history panel is closed', () => {
-    cy.visit('/?queryy={test}');
+    cy.visit('/?query={test}');
     cy.clickExecuteQuery();
     cy.get('button[aria-label="Show History"]').click();
     cy.get('ul.graphiql-history-items li').should('have.length', 1);
   });
 
   it('will not save invalid queries', () => {
-    cy.visit(`?queryy=${mockBadQuery}`);
+    cy.visit(`?query=${mockBadQuery}`);
     cy.get('button[aria-label="Show History"]').click();
     cy.clickExecuteQuery();
     cy.get('ul.graphiql-history-items li').should('have.length', 0);
   });
 
   it('will save if new query is different than previous query', () => {
-    cy.visit(`?queryy=${mockQuery1}&headers=${mockHeaders1}`);
+    cy.visit(`?query=${mockQuery1}&headers=${mockHeaders1}`);
     cy.get('button[aria-label="Show History"]').click();
     cy.clickExecuteQuery();
     cy.get('ul.graphiql-history-items li').should('have.length', 1);
 
-    cy.visit(`?queryy=${mockQuery2}&headers=${mockHeaders1}`);
+    cy.visit(`?query=${mockQuery2}&headers=${mockHeaders1}`);
     cy.clickExecuteQuery();
     cy.get('ul.graphiql-history-items li').should('have.length', 2);
   });
 
   it('will not save if new query is the same as previous query', () => {
-    cy.visit(`?queryy=${mockQuery1}&headers=${mockHeaders1}`);
+    cy.visit(`?query=${mockQuery1}&headers=${mockHeaders1}`);
     cy.get('button[aria-label="Show History"]').click();
     cy.clickExecuteQuery();
     cy.get('ul.graphiql-history-items li').should('have.length', 1);
 
-    cy.visit(`?queryy=${mockQuery1}&headers=${mockHeaders1}`);
+    cy.visit(`?query=${mockQuery1}&headers=${mockHeaders1}`);
     cy.clickExecuteQuery();
     cy.get('ul.graphiql-history-items li').should('have.length', 1);
   });
 
   it('will save query if the variables change', () => {
     cy.visit(
-      `?queryy=${mockQuery1}&headers=${mockHeaders1}&defaultVariables=${mockVariables1}`,
+      `?query=${mockQuery1}&headers=${mockHeaders1}&defaultVariables=${mockVariables1}`,
     );
     cy.get('button[aria-label="Show History"]').click();
     cy.clickExecuteQuery();
     cy.get('ul.graphiql-history-items li').should('have.length', 1);
 
     cy.visit(
-      `?queryy=${mockQuery1}&headers=${mockHeaders1}&defaultVariables=${mockVariables2}`,
+      `?query=${mockQuery1}&headers=${mockHeaders1}&defaultVariables=${mockVariables2}`,
     );
     cy.clickExecuteQuery();
     cy.get('ul.graphiql-history-items li').should('have.length', 2);
   });
 
   it('will save query if the headers change', () => {
-    cy.visit(`?queryy=${mockQuery1}&headers=${mockHeaders1}`);
+    cy.visit(`?query=${mockQuery1}&headers=${mockHeaders1}`);
     cy.get('button[aria-label="Show History"]').click();
     cy.clickExecuteQuery();
     cy.get('ul.graphiql-history-items li').should('have.length', 1);
 
-    cy.visit(`?queryy=${mockQuery1}&headers=${mockHeaders2}`);
+    cy.visit(`?query=${mockQuery1}&headers=${mockHeaders2}`);
     cy.clickExecuteQuery();
     cy.get('ul.graphiql-history-items li').should('have.length', 2);
   });
 
   it('should remove individual item', () => {
-    cy.visit(`?queryy=${mockQuery1}&headers=${mockHeaders1}`);
+    cy.visit(`?query=${mockQuery1}&headers=${mockHeaders1}`);
     cy.clickExecuteQuery();
-    cy.visit(`?queryy=${mockQuery2}&headers=${mockHeaders1}`);
+    cy.visit(`?query=${mockQuery2}&headers=${mockHeaders1}`);
     cy.clickExecuteQuery();
     cy.get('button[aria-label="Show History"]').click();
 
@@ -99,9 +99,9 @@ describe('history', () => {
   });
 
   it('should remove all items', () => {
-    cy.visit(`?queryy=${mockQuery1}&headers=${mockHeaders1}`);
+    cy.visit(`?query=${mockQuery1}&headers=${mockHeaders1}`);
     cy.clickExecuteQuery();
-    cy.visit(`?queryy=${mockQuery2}&headers=${mockHeaders1}`);
+    cy.visit(`?query=${mockQuery2}&headers=${mockHeaders1}`);
     cy.clickExecuteQuery();
     cy.get('button[aria-label="Show History"]').click();
     cy.get('ul.graphiql-history-items li').should('have.length', 2);
@@ -111,9 +111,9 @@ describe('history', () => {
   });
 
   it('should add/remove item to favorite', () => {
-    cy.visit(`?queryy=${mockQuery1}&headers=${mockHeaders1}`);
+    cy.visit(`?query=${mockQuery1}&headers=${mockHeaders1}`);
     cy.clickExecuteQuery();
-    cy.visit(`?queryy=${mockQuery2}&headers=${mockHeaders1}`);
+    cy.visit(`?query=${mockQuery2}&headers=${mockHeaders1}`);
     cy.clickExecuteQuery();
     cy.get('button[aria-label="Show History"]').click();
     cy.get('ul.graphiql-history-items li').should('have.length', 2);
