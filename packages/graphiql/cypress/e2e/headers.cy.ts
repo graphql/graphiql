d50f84f4334506d997a01b53df683e3218e3c06d
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-06-16 02:11:53 +0200

    upd

--- packages/graphiql/cypress/e2e/headers.cy.ts
+++ packages/graphiql/cypress/e2e/headers.cy.ts
@@ -1,14 +1,12 @@
 const DEFAULT_HEADERS = '{"foo":2}';
 
 describe('Headers', () => {
-  describe('`defaultHeaders`', () => {
-    it('should have default query only on first tab and default headers in all tabs', () => {
-      cy.visit(`?defaultQuery={test}&defaultHeaders=${DEFAULT_HEADERS}`);
-      cy.assertHasValues({ query: '{test}', headersString: DEFAULT_HEADERS });
-      cy.get('.graphiql-tab-add').click();
-      cy.assertHasValues({ query: '', headersString: DEFAULT_HEADERS });
-      cy.get('.graphiql-tab-add').click();
-      cy.assertHasValues({ query: '', headersString: DEFAULT_HEADERS });
-    });
+  it('should have default query only on first tab and default headers in all tabs', () => {
+    cy.visit(`?defaultQuery={test}&defaultHeaders=${DEFAULT_HEADERS}`);
+    cy.assertHasValues({ query: '{test}', headersString: DEFAULT_HEADERS });
+    cy.get('.graphiql-tab-add').click();
+    cy.assertHasValues({ query: '', headersString: DEFAULT_HEADERS });
+    cy.get('.graphiql-tab-add').click();
+    cy.assertHasValues({ query: '', headersString: DEFAULT_HEADERS });
   });
 });
