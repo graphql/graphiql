64940b4fc7fce334cfdaf512cd98373625fe82fd
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-06-20 12:34:25 +0200

    upd

--- packages/graphiql-plugin-doc-explorer/src/components/schema-documentation.tsx
+++ packages/graphiql-plugin-doc-explorer/src/components/schema-documentation.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import type { FC } from 'react';
 import type { GraphQLSchema } from 'graphql';
 import { MarkdownContent } from '@graphiql/react';
 import { ExplorerSection } from './section';
--- packages/graphiql/src/ui/footer.tsx
+++ packages/graphiql/src/ui/footer.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from 'react';
+import type { FC, ReactNode } from 'react';
 
 // Configure the UI by providing this Component as a child of GraphiQL.
 export const GraphiQLFooter: FC<{ children: ReactNode }> = ({ children }) => {
