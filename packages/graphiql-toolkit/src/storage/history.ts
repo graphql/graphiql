25a7af8165083ca9856df57be79f6e78ab24ef5f
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-06-22 18:28:24 +0200

    upd

--- packages/graphiql-toolkit/src/storage/history.ts
+++ packages/graphiql-toolkit/src/storage/history.ts
@@ -1,6 +1,5 @@
 import { parse } from 'graphql';
-
-import { StorageAPI } from './base';
+import type { PersistStorage } from 'zustand/middleware';
 import { QueryStore, QueryStoreItem } from './query';
 
 const MAX_QUERY_SIZE = 100000;
@@ -11,7 +10,7 @@ export class HistoryStore {
   favorite: QueryStore;
 
   constructor(
-    private storage: StorageAPI,
+    private storage: PersistStorage<any>,
     private maxHistoryLength: number,
   ) {
     this.history = new QueryStore(
--- packages/graphiql-toolkit/src/storage/index.ts
+++ packages/graphiql-toolkit/src/storage/index.ts
@@ -1,4 +1,2 @@
-export * from './base';
 export * from './history';
 export * from './query';
-export * from './custom';
