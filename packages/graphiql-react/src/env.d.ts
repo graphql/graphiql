6da571057732f97cb70876a2358eba81a2f3d093
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-07-13 04:41:41 +0200

    upd

--- packages/graphiql-react/src/env.d.ts
+++ packages/graphiql-react/src/env.d.ts
@@ -1,6 +1,20 @@
 declare namespace globalThis {
-  import * as monaco from './monaco-editor';
+  import type * as monaco from 'monaco-editor';
   var MonacoEnvironment: monaco.Environment;
   // Needs for cypress
   var __MONACO = monaco;
+}
+
+declare module 'monaco-editor/esm/vs/editor/common/standalone/standaloneEnums.js' {
+  // eslint-disable-next-line @typescript-eslint/no-restricted-imports
+  export { KeyCode } from 'monaco-editor';
+}
+
+declare module 'monaco-editor/esm/vs/editor/common/services/editorBaseApi.js' {
+  // eslint-disable-next-line @typescript-eslint/no-restricted-imports
+  export { KeyMod } from 'monaco-editor';
+}
+declare module 'monaco-editor/esm/vs/base/common/uri.js' {
+  // eslint-disable-next-line @typescript-eslint/no-restricted-imports
+  export { Uri as URI } from 'monaco-editor';
 }
