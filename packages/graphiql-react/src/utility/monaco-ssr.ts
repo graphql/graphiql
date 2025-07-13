94884cfda4ee02bb76ca0f2adc27077753f307bf
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-07-13 06:27:26 +0200

    Update packages/graphiql-react/src/utility/monaco-ssr.ts

--- packages/graphiql-react/src/utility/monaco-ssr.ts
+++ packages/graphiql-react/src/utility/monaco-ssr.ts
@@ -1,5 +1,5 @@
 /**
- * Re‑export Monaco Editor enums.
+ * Re‑export Monaco Editor exports.
  * Importing directly from 'monaco-editor' causes a “window is not defined” error during SSR.
  */
 export { KeyCode } from 'monaco-editor/esm/vs/editor/common/standalone/standaloneEnums.js';
