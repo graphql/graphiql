1e3868a63a07a23b7ccfaedc4cded25739866515
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-07-17 22:56:54 +0200

    prettier

--- examples/graphiql-vite-react-router/app/root.tsx
+++ examples/graphiql-vite-react-router/app/root.tsx
@@ -4,18 +4,18 @@ import { Links, Meta, Outlet, Scripts, ScrollRestoration } from 'react-router';
 const Root: FC = () => {
   return (
     <html lang="en">
-    <head>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width,initial-scale=1" />
-      <Meta />
-      <Links />
-    </head>
-    <body>
-    <noscript>You need to enable JavaScript to run this app.</noscript>
-    <Outlet />
-    <ScrollRestoration />
-    <Scripts />
-    </body>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <noscript>You need to enable JavaScript to run this app.</noscript>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+      </body>
     </html>
   );
 };
