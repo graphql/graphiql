fbe46f0beef1b68053b7a487a9e1939ad179c94a
Trevor Scheer <trevor.scheer@gmail.com>
2026-06-05 16:31:55 -0700

    use method shorthand in localStorage polyfill

--- packages/graphiql-toolkit/setup-files.ts
+++ packages/graphiql-toolkit/setup-files.ts
@@ -4,13 +4,13 @@ if (typeof localStorage === 'undefined') {
   const store = new Map<string, string>();
   globalThis.localStorage = {
     getItem: (key: string) => store.get(key) ?? null,
-    setItem: (key: string, value: string) => {
+    setItem(key: string, value: string) {
       store.set(key, value);
     },
-    removeItem: (key: string) => {
+    removeItem(key: string) {
       store.delete(key);
     },
-    clear: () => {
+    clear() {
       store.clear();
     },
     get length() {
