39a95c2a131a73f8e3c95dc9a6905d7d080b1363
Trevor Scheer <trevor.scheer@gmail.com>
2026-09-06 10:26:29 -0700

    Reproduce the E2E subscription origin mismatch

--- packages/graphiql-e2e/cypress/e2e/graphql-ws.cy.ts
+++ packages/graphiql-e2e/cypress/e2e/graphql-ws.cy.ts
@@ -8,10 +8,36 @@ describe('IncrementalDelivery support via fetcher', () => {
       },
     };
 
-    it('Expects a subscription to resolve', () => {
-      cy.visitWithOp({ query: testSubscription, variables: { delay: 0 } });
+    it('Uses the page origin for subscriptions', () => {
+      let socketUrl: string | undefined;
+      const query = encodeURIComponent(testSubscription);
+      const variables = encodeURIComponent(JSON.stringify({ delay: 0 }));
+
+      cy.visit(`?query=${query}&variables=${variables}`, {
+        onBeforeLoad(win) {
+          const NativeWebSocket = win.WebSocket;
+          const ObservedWebSocket = new Proxy(NativeWebSocket, {
+            construct(Target, args) {
+              socketUrl = String(args[0]);
+              return Reflect.construct(Target, args);
+            },
+          });
+          Object.defineProperty(win, 'WebSocket', {
+            configurable: true,
+            value: ObservedWebSocket,
+          });
+        },
+      });
       cy.get('.monaco-editor').should('have.length', 4);
       cy.clickExecuteQuery();
+      cy.location().then(({ protocol, host }) => {
+        const socketProtocol = protocol === 'https:' ? 'wss:' : 'ws:';
+        cy.window().should(() => {
+          expect(socketUrl).to.equal(
+            `${socketProtocol}//${host}/subscriptions`,
+          );
+        });
+      });
       cy.assertQueryResult(mockSubscriptionSuccess);
     });
   });
