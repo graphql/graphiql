6a0f48e572540693aa1bc97eac735a5f4d8585e5
Trevor Scheer <trevor.scheer@gmail.com>
2026-09-12 22:54:07 -0700

    use the v4 get-value export

--- packages/graphiql-react/src/stores/execution.ts
+++ packages/graphiql-react/src/stores/execution.ts
@@ -11,7 +11,7 @@ import {
 import { ExecutionResult, GraphQLError, print } from 'graphql';
 import { getFragmentDependenciesForAST } from 'graphql-language-service';
 import setValue from 'set-value';
-import getValue from 'get-value';
+import { getValue } from 'get-value';
 
 import type { StateCreator } from 'zustand';
 import { tryParseJSONC, Range } from '../utility';
